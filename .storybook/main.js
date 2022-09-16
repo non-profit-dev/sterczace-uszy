d37290f08578f64acc863376d2e2c74f5854d911
Amadriella <amadriella@Amadriellas-MacBook-Pro.local>
2022-09-16 13:06:07 +0200

    feat-fsu-14-storybook-config

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../packages/design-system/stories/**/*.stories.mdx",
+    "../packages/design-system/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
