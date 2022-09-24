b4d78bf92c96ed54e7ee3ae9e6bb61668d97c8c7
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-09-24 17:00:42 +0200

    google fonts added in frontend folder

--- packages/frontend/app/pages/index.js
+++ packages/frontend/app/pages/index.js
@@ -3,18 +3,29 @@ import Articles from "../components/articles"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { fetchAPI } from "../lib/api"
+import Head from "next/head"
 
 const Home = ({ articles, categories, homepage }) => {
   return (
-    <Layout categories={categories}>
-      <Seo seo={homepage.attributes.seo} />
-      <div className="uk-section">
-        <div className="uk-container uk-container-large">
-          <h1>{homepage.attributes.hero.title}</h1>
-          <Articles articles={articles} />
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Layout categories={categories}>
+        <Seo seo={homepage.attributes.seo} />
+        <div className="uk-section">
+          <div className="uk-container uk-container-large">
+            <h1>{homepage.attributes.hero.title}</h1>
+            <Articles articles={articles} />
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
