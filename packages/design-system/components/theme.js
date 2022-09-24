86cd088f86d4e5829f8062ded3095c541a7bafbb
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-09-24 17:00:01 +0200

    theme update

--- packages/design-system/components/theme.js
+++ packages/design-system/components/theme.js
@@ -1,23 +1,162 @@
 const theme = {
-	typography: {},
-	containerHeight: '100vh',
-	defaultPadding: '16px',
-	fontStyles: {
-		size: 16,
-		color: '#FFF',
-		family: 'Josefin Sans',
-	},
-	colors: {
-		primaryOne: '#041E42',
-		primaryTwo: '#003594',
-		primaryThree: '#fff',
-		secondaryOne: '#869397',
-		secondaryTwo: '#7F9695',
-	},
-	spacings: {
-		text: 24,
-		image: 64,
-	},
+  containerHeight: "100vh",
+  defaultPadding: "16px",
+  fontStyles: {
+    primaryFontFamily: "'Poppins', sans-serif",
+  },
+  colors: {
+    primary: {
+      100: "#FFFEFE",
+      200: "#222222",
+      300: "#C1121F",
+      400: "#020202",
+    },
+    secondary: {
+      100: "#FEF8EC",
+      200: "#F4CEC3",
+    },
+    grey: {
+      100: "#EBEBEB",
+      200: "#DFDFDF",
+      300: "#C4C4C4",
+      400: "#818181",
+      500: "#414141",
+    },
+    tint: {
+      100: "#FCDDDF",
+      200: "#F9BBBF",
+      300: "#F5989F",
+      400: "#EF5460",
+    },
+    lightTint: {
+      100: "#FEF9F8",
+      200: "#FBEDE8",
+      300: "#F8DFD8",
+    },
+  },
+  spacings: {
+    xxsmall: "4px",
+    xsmall: "8px",
+    small: "12px",
+    medium: "16px",
+    large: "24px",
+    xlarge: "48px",
+    xxlarge: "80px",
+    xxxlarge: "120px",
+  },
+  typography: {
+    desktop: {
+      h1: {
+        weight: 600,
+        size: "60px",
+        lineHeight: "72px",
+      },
+      h2: {
+        weight: 600,
+        size: "48px",
+        lineHeight: "58px",
+      },
+      h3: {
+        weight: 600,
+        size: "36px",
+        lineHeight: "43px",
+      },
+      h4: {
+        weight: 600,
+        size: "28px",
+        lineHeight: "34px",
+      },
+      h5: {
+        weight: 600,
+        size: "24px",
+        lineHeight: "29px",
+      },
+      h6: {
+        weight: 600,
+        size: "20px",
+        lineHeight: "24px",
+      },
+      bodyLarge: {
+        weight: 400,
+        size: "28px",
+        lineHeight: "34px",
+      },
+      bodyMedium: {
+        weight: 400,
+        size: "20px",
+        lineHeight: "24px",
+      },
+      bodySmall: {
+        weight: 400,
+        size: "16px",
+        lineHeight: "19px",
+      },
+      bodyTitle: {
+        weight: 400,
+        size: "20px",
+        lineHeight: "24px",
+      },
+      bodySubtitle: {
+        weight: 400,
+        size: "16px",
+        lineHeight: "19px",
+      },
+      bodySmallBold: {
+        weight: 700,
+        size: "16px",
+        lineHeight: "19px",
+      },
+    },
+    tablet: {
+      h1: {
+        weight: 600,
+        size: "50px",
+        lineHeight: "75px",
+      },
+      h2: {
+        weight: 600,
+        size: "40px",
+        lineHeight: "60px",
+      },
+      h3: {
+        weight: 600,
+        size: "30px",
+        lineHeight: "45px",
+      },
+      h4: {
+        weight: 600,
+        size: "24px",
+        lineHeight: "36px",
+      },
+      h5: {
+        weight: 600,
+        size: "22px",
+        lineHeight: "33px",
+      },
+      h6: {
+        weight: 600,
+        size: "20px",
+        lineHeight: "30px",
+      },
+    },
+    mobile: {
+      h1: {
+        weight: 700,
+        size: "36px",
+        lineHeight: "54px",
+      },
+      h2: {
+        weight: 600,
+        size: "16px",
+        lineHeight: "19px",
+      },
+      h3: {
+        weight: 500,
+        size: "12px",
+        lineHeight: "14px",
+      },
+    },
+  },
 }
 
 export { theme }
