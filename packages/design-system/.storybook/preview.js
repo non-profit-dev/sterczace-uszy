d20fde983f74347ea6f0cde487ee643fa2389bf1
Amanda <97829893+AmandaKr@users.noreply.github.com>
2022-09-25 12:26:23 +0200

    feat: normalize styles (#9)

--- packages/design-system/.storybook/preview.js
+++ packages/design-system/.storybook/preview.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import theme from '../components/theme'
+import React from "react"
+import { ThemeProvider } from "styled-components"
+import theme from "../components/theme"
+import { Normalize } from "styled-normalize"
 
 export const parameters = {
-	actions: { argTypesRegex: '^on[A-Z].*' },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
-		},
-	},
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 }
 
 export const decorators = [
-	Story => (
-		<ThemeProvider theme={theme}>
-			<Story />
-		</ThemeProvider>
-	),
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <Story />
+    </ThemeProvider>
+  ),
 ]
