d06c07ce59d45029b6d97b2787a26cabafac1535
Klaudia <78494852+im-klau@users.noreply.github.com>
2022-09-26 10:58:29 +0200

    test: replace heart icon with mock icon

--- packages/design-system/components/component/__tests__/Component.test.jsx
+++ packages/design-system/components/component/__tests__/Component.test.jsx
@@ -8,21 +8,7 @@ import Component from "../Component"
 
 const heading = "Heading"
 const description = "Description"
-const HeartIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-  </svg>
-)
+const MockIcon = () => <span>icon</span>
 
 // Group tests
 describe(`Component`, () => {
@@ -66,7 +52,7 @@ describe(`Component`, () => {
       <Component
         heading={heading}
         description={description}
-        button={{ icon: HeartIcon, onClick: jest.fn() }}
+        button={{ icon: MockIcon, onClick: jest.fn() }}
       />
     )
   })
@@ -78,7 +64,7 @@ describe(`Component`, () => {
       <Component
         heading={heading}
         description={description}
-        button={{ icon: HeartIcon, onClick: onClickMock }}
+        button={{ icon: MockIcon, onClick: onClickMock }}
       />
     )
 
