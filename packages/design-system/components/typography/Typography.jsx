765395b5038a594a596507a9a6b34bbe8f8a1572
unknown <amanda.krempa1@gmail.com>
2022-10-05 22:41:16 +0200

    fix: delete duplicate color

--- packages/design-system/components/typography/Typography.jsx
+++ packages/design-system/components/typography/Typography.jsx
@@ -53,7 +53,6 @@ Typography.propTypes = {
   ]),
   variant: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6", "p"]),
   as: PropTypes.string,
-  color: PropTypes.string,
   MediaQueryList: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
   children: PropTypes.node,
 }
