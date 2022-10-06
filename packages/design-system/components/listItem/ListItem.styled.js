67792ce6007d1172f906a8f979ef89a17c8b8be7
Amadriella <nconnect1789@gmail.com>
2022-10-06 14:51:28 +0200

    feat: fsu-49 add styles to ListItem.styled file

--- packages/design-system/components/listItem/ListItem.styled.js
+++ packages/design-system/components/listItem/ListItem.styled.js
@@ -3,10 +3,12 @@ import theme from "../theme/theme"
 
 export const ListItem = styled.div`
   flex-direction: row;
+  flex: 1;
   justify-content: flex-start;
   background-color: "blue";
   border-width: "2px";
-  border-color: orange;
+  border-color: ${({ variant }) =>
+    variant === "primary" ? theme.colors.primary[300] : theme.colors.grey[300]};
   color: ${({ variant }) =>
     variant === "primary" ? theme.colors.primary[500] : theme.colors.grey[600]};
 `
