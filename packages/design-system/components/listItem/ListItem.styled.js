9053ad93137b3ee40399eaef7da8a40c17fda8a9
Amadriella <nconnect1789@gmail.com>
2022-10-10 09:23:49 +0200

    feat: fsu-49 add simple ListItem component tests

--- packages/design-system/components/listItem/ListItem.styled.js
+++ packages/design-system/components/listItem/ListItem.styled.js
@@ -2,13 +2,37 @@ import styled from "@emotion/styled"
 import theme from "../theme/theme"
 
 export const ListItem = styled.div`
-  flex-direction: row;
   flex: 1;
+  flex-direction: row;
   justify-content: flex-start;
-  background-color: "blue";
-  border-width: "2px";
-  border-color: ${({ variant }) =>
-    variant === "primary" ? theme.colors.primary[300] : theme.colors.grey[300]};
+  background-color: "transparent";
+  border-width: ${({ variant }) => (variant === "primary" ? "3px" : "5px")};
+  border-style: "solid";
+  border-color: ${({ variant }) => (variant === "primary" ? "red" : "blue")};
   color: ${({ variant }) =>
     variant === "primary" ? theme.colors.primary[500] : theme.colors.grey[600]};
+  line-height: "22,4px";
+  ${({ size }) => {
+    switch (size) {
+      case "small":
+        return theme.typography.desktop.bodySubtitle
+      case "medium":
+        return theme.typography.desktop.bodySmall
+      case "large":
+        return theme.typography.desktop.bodyMedium
+      default:
+        return theme.typography.desktop.bodySmall
+    }
+  }};
+`
+export const ListItemIcon = styled.span`
+  padding: 2px;
+  height: "22,4px";
+  position: absolute;
+  display: inline;
+  align-items: center;
+  justify-content: center;
+  svg {
+    padding: "3px";
+  }
 `
