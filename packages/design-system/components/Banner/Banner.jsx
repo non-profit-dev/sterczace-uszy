5c647ef6b0ecb203c771f77a3e9788fa176df3a3
Emilia123x <101749808+Emilia123x@users.noreply.github.com>
2022-10-12 17:05:31 +0200

    chore: corrected component, deleted onclick function form the icon

--- packages/design-system/components/Banner/Banner.jsx
+++ packages/design-system/components/Banner/Banner.jsx
@@ -1,26 +1,14 @@
-import { useState } from "react"
-import { string, shape, func, node } from "prop-types"
+import { string, shape, node } from "prop-types"
 
 import * as Styled from "./Banner.styled"
 
-const Banner = ({ heading, link, button }) => {
-  const [onClick, setonClick] = useState()
-
-  const handleClick = () => {
-    setonClick(!onClick)
-    button.onClick()
-  }
-
-  return (
-    <Styled.Banner>
-      <Styled.Heading>{heading}</Styled.Heading>
-      {link && <Styled.Link href={link.href}>{link.text}</Styled.Link>}
-      {button && (
-        <Styled.Button onClick={handleClick}>{button.icon}</Styled.Button>
-      )}
-    </Styled.Banner>
-  )
-}
+const Banner = ({ heading, link, button }) => (
+  <Styled.Banner>
+    <Styled.Heading>{heading}</Styled.Heading>
+    {link && <Styled.Link href={link.href}>{link.text}</Styled.Link>}
+    {button && <Styled.Button>{button.icon}</Styled.Button>}
+  </Styled.Banner>
+)
 
 Banner.propTypes = {
   heading: string.isRequired,
@@ -30,7 +18,6 @@ Banner.propTypes = {
   }),
   button: shape({
     icon: node,
-    onClick: func,
   }),
 }
 
