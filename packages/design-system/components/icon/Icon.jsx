06af7bd3fd1a279859461ec4b296d38219dc171e
Paulina Szczepanik (Grymulska) <87578610+paulgrym@users.noreply.github.com>
2022-10-15 19:11:27 +0200

    feat: Button component (#19)
    Co-authored-by: Klaudia <78494852+im-klau@users.noreply.github.com>

--- packages/design-system/components/icon/Icon.jsx
+++ packages/design-system/components/icon/Icon.jsx
@@ -1,6 +1,8 @@
 import { string, oneOf } from "prop-types"
 
 import Heart from "../../icons/Heart.svg"
+import ArrowDown from "../../icons/ArrowDown.svg"
+import Download from "../../icons/Download.svg"
 
 const sizes = {
   small: 16,
@@ -8,8 +10,10 @@ const sizes = {
   large: 30,
 }
 
-const icons = {
+export const icons = {
   heart: Heart,
+  arrowDown: ArrowDown,
+  download: Download,
 }
 
 const Icon = ({ name, color, size }) => {
