25dbc2a0cab4599a766efbdeaf9a85d4c4ad3781
Klaudia <78494852+im-klau@users.noreply.github.com>
2022-10-16 11:45:33 +0200

    fix(banner): reuse button component

--- packages/design-system/components/banner/Banner.jsx
+++ packages/design-system/components/banner/Banner.jsx
@@ -1,28 +1,20 @@
-import { string, shape, node } from "prop-types"
+import { string, node } from "prop-types"
 
 import * as Styled from "./Banner.styled"
 
-const Banner = ({ heading, link, button }) => (
+const Banner = ({ heading, button }) => (
   <Styled.Banner>
     <Styled.Heading>{heading}</Styled.Heading>
-    {link && <Styled.Link href={link.href}>{link.text}</Styled.Link>}
-    {button && <Styled.Button>{button.icon}</Styled.Button>}
+    {button && button}
   </Styled.Banner>
 )
 
 Banner.propTypes = {
   heading: string.isRequired,
-  link: shape({
-    text: string,
-    href: string,
-  }),
-  button: shape({
-    icon: node,
-  }),
+  button: node,
 }
 
 Banner.defaultProps = {
-  link: null,
   button: null,
 }
 
