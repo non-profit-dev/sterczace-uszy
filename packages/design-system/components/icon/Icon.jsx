9c02924428caf6f331edfe8554c10210aacf0265
Amadriella <nconnect1789@gmail.com>
2022-10-17 13:40:41 +0200

    feat: fsu-33-icons add list, link and contact icons

--- packages/design-system/components/icon/Icon.jsx
+++ packages/design-system/components/icon/Icon.jsx
@@ -1,25 +1,39 @@
 import { string, oneOf } from "prop-types"
 
-import Heart from "../../icons/Heart.svg"
 import ArrowDown from "../../icons/ArrowDown.svg"
-import Download from "../../icons/Download.svg"
 import ArrowRight from "../../icons/ArrowRight.svg"
+import ChevronRight from "../../icons/ChevronRight.svg"
+import Done from "../../icons/Done.svg"
 import DoubleArrow from "../../icons/DoubleArrow.svg"
+import Download from "../../icons/Download.svg"
+import Facebook from "../../icons/Facebook.svg"
+import Heart from "../../icons/Heart.svg"
+import Instagram from "../../icons/Instagram.svg"
+import Location from "../../icons/Location.svg"
+import Mail from "../../icons/Mail.svg"
+import Phone from "../../icons/Phone.svg"
 
 const sizes = {
   small: 16,
   medium: 24,
   large: 30,
-  // xlarge: 30 x 48,
-  // xxlarge: 23 x 36,
+  xlarge: 48,
+  xxlarge: 36,
 }
 
 export const icons = {
-  heart: Heart,
   arrowDown: ArrowDown,
-  download: Download,
   arrowRight: ArrowRight,
+  chevronRight: ChevronRight,
+  done: Done,
   doubleArrow: DoubleArrow,
+  download: Download,
+  facebook: Facebook,
+  heart: Heart,
+  instagram: Instagram,
+  mail: Mail,
+  location: Location,
+  phone: Phone,
 }
 
 const Icon = ({ name, color, size }) => {
