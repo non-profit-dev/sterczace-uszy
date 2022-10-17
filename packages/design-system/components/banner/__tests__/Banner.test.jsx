55fa3e1dde84c64859efad2d562e29ff1379c5c4
Emilia123x <101749808+Emilia123x@users.noreply.github.com>
2022-10-17 10:54:20 +0200

    Update Banner.test.jsx
    git commit -m"corrected test file to see If Banner renders with default properties"

--- packages/design-system/components/banner/__tests__/Banner.test.jsx
+++ packages/design-system/components/banner/__tests__/Banner.test.jsx
@@ -1,11 +1,12 @@
-import { render, screen, } from "../../../test-utils"
+import { render, screen, act } from "../../../test-utils"
 
 import Banner from "../Banner"
 
 const heading = "heading"
 
-describe(Banner, () => {
-  it(renders heading with default properties, () => {
+
+describe(`Banner`, () => {
+  it(`renders Banner with default properties`, () => {
     render(<Banner heading={heading} />)
     expect(screen.getByText(heading)).toBeInTheDocument()
   })
