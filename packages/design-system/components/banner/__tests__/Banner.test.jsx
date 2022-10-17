c272a6b099fc5c571a9415b1b50d8dfba9a19d52
Klaudia <78494852+im-klau@users.noreply.github.com>
2022-10-17 18:27:34 +0200

    fix: prettier issues

--- packages/design-system/components/banner/__tests__/Banner.test.jsx
+++ packages/design-system/components/banner/__tests__/Banner.test.jsx
@@ -4,11 +4,9 @@ import Banner from "../Banner"
 
 const heading = "heading"
 
-
 describe(`Banner`, () => {
   it(`renders Banner with default properties`, () => {
     render(<Banner heading={heading} />)
     expect(screen.getByText(heading)).toBeInTheDocument()
   })
-
 })
