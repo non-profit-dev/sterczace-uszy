d6d51c4a89ea68e03f724dbf2d2fdaa35888a25f
Amadriella <nconnect1789@gmail.com>
2022-10-17 10:40:28 +0200

    feat: add new icons

--- packages/design-system/components/icon/Icon.jsx
+++ packages/design-system/components/icon/Icon.jsx
@@ -3,17 +3,23 @@ import { string, oneOf } from "prop-types"
 import Heart from "../../icons/Heart.svg"
 import ArrowDown from "../../icons/ArrowDown.svg"
 import Download from "../../icons/Download.svg"
+import ArrowRight from "../../icons/ArrowRight.svg"
+import DoubleArrow from "../../icons/DoubleArrow.svg"
 
 const sizes = {
   small: 16,
   medium: 24,
   large: 30,
+  // xlarge: 30 x 48,
+  // xxlarge: 23 x 36,
 }
 
 export const icons = {
   heart: Heart,
   arrowDown: ArrowDown,
   download: Download,
+  arrowRight: ArrowRight,
+  doubleArrow: DoubleArrow,
 }
 
 const Icon = ({ name, color, size }) => {
