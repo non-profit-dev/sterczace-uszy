df5ac02070c3a17d3bc444acaa10ca4edffc9ab4
Emilia123x <101749808+Emilia123x@users.noreply.github.com>
2022-10-17 10:30:52 +0200

    Update Banner.test.jsx
    git commit -m"deleted unnecessary imports from test file"

--- packages/design-system/components/banner/__tests__/Banner.test.jsx
+++ packages/design-system/components/banner/__tests__/Banner.test.jsx
@@ -1,14 +1,11 @@
-import userEvent from "@testing-library/user-event"
-
-import { render, screen, act } from "../../../test-utils"
+import { render, screen, } from "../../../test-utils"
 
 import Banner from "../Banner"
 
 const heading = "heading"
 
-
-describe(`Banner`, () => {
-  it(`renders heading with default properties`, () => {
+describe(Banner, () => {
+  it(renders heading with default properties, () => {
     render(<Banner heading={heading} />)
     expect(screen.getByText(heading)).toBeInTheDocument()
   })
