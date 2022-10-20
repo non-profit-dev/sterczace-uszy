a6cee57d8e1a60a6c0c20df6a2c3169093a60d28
Emilia123x <101749808+Emilia123x@users.noreply.github.com>
2022-10-20 09:36:14 +0200

    Update Banner.test.jsx
    git commit -m "ci: fixed test, added new test for button"

--- packages/design-system/components/banner/__tests__/Banner.test.jsx
+++ packages/design-system/components/banner/__tests__/Banner.test.jsx
@@ -3,10 +3,15 @@ import { render, screen } from "../../../test-utils"
 import Banner from "../Banner"
 
 const heading = "heading"
+const button = "button"
 
 describe(`Banner`, () => {
   it(`renders Banner with default properties`, () => {
     render(<Banner heading={heading} />)
     expect(screen.getByText(heading)).toBeInTheDocument()
+  })
+  it(`renders Button with default properties`, () => {
+    render(<Banner button={button} />)
+    expect(screen.getByText(button)).toBeInTheDocument()
   })
 })
