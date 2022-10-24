c864ffbfa07649fdcee658b199e75b7580859e43
Emilia123x <101749808+Emilia123x@users.noreply.github.com>
2022-10-24 14:07:33 +0200

    feat: banner (#28)
    Co-authored-by: Klaudia <78494852+im-klau@users.noreply.github.com>

---
+++ packages/design-system/components/banner/Banner.styled.js
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+export const Banner = styled.div`
+  text-align: center;
+  padding: 19px;
+  position: relative;
+  color: ${({ theme }) => theme.colors.grey[600]};
+  background-color: ${({ theme }) => theme.colors.complementary[100]};
+`
+
+export const Heading = styled.h2`
+  text-align: center;
+  display: inline;
+  margin: 0;
+  ${({ theme }) => theme.typography.desktop.bodySmall};
+`
---
+++ packages/design-system/components/banner/__tests__/Banner.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "../../../test-utils"
+
+import Banner from "../Banner"
+
+const heading = "heading"
+const button = "button"
+
+describe(`Banner`, () => {
+  it(`renders Banner with default properties`, () => {
+    render(<Banner heading={heading} />)
+    expect(screen.getByText(heading)).toBeInTheDocument()
+  })
+  it(`renders Button with default properties`, () => {
+    render(<Banner button={button} heading={heading} />)
+    expect(screen.getByText(button)).toBeInTheDocument()
+  })
+})
