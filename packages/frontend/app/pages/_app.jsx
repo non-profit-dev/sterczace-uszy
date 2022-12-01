5006eddf83bb459a65b4cbb0b0507d9b9d366370
Klaudia <78494852+im-klau@users.noreply.github.com>
2022-12-01 13:40:02 +0100

    feat: introduce next.js pages auto deploy to staging env in vercel (#51)

--- packages/frontend/app/pages/_app.jsx
+++ packages/frontend/app/pages/_app.jsx
@@ -1,52 +1,24 @@
-import App from "next/app"
 import Head from "next/head"
-import { createContext } from "react"
 import { Normalize } from "styled-normalize"
 import { ThemeProvider } from "@emotion/react"
 import { node, shape } from "prop-types"
-
-import { fetchAPI } from "frontend/lib/api"
+import { createContext } from "react"
 
 import theme from "design-system/theme"
 
-// Store Strapi Global object in context
-export const GlobalContext = createContext({})
-
-const MyApp = ({ Component, pageProps }) => {
-  const { global } = pageProps
-
-  return (
-    <>
-      <Head />
-      <ThemeProvider theme={theme}>
-        <GlobalContext.Provider value={global.attributes}>
-          <Normalize />
-          <Component {...pageProps} />
-        </GlobalContext.Provider>
-      </ThemeProvider>
-    </>
-  )
-}
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+)
 
 MyApp.propTypes = {
   Component: node.isRequired,
   pageProps: shape({}).isRequired,
-}
-
-MyApp.getInitialProps = async (ctx) => {
-  // Calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(ctx)
-  // Fetch global site settings from Strapi
-  const globalRes = await fetchAPI("/global", {
-    populate: {
-      favicon: "*",
-      defaultSeo: {
-        populate: "*",
-      },
-    },
-  })
-  // Pass the data to our page via props
-  return { ...appProps, pageProps: { global: globalRes.data } }
 }
 
 export default MyApp
