533af2ac9e6ed59d2a60d9f54e8ce6b6f8eb77bc
Klaudia <78494852+im-klau@users.noreply.github.com>
2022-12-01 13:13:54 +0100

    feat: global eslint config (#50)

--- packages/frontend/app/pages/index.jsx
+++ packages/frontend/app/pages/index.jsx
@@ -1,47 +1,21 @@
 import React from "react"
 import { Normalize } from "styled-normalize"
 
-import { fetchAPI } from "frontend/lib/api"
 import Head from "next/head"
 
 import Badge from "design-system/components/badge"
 
-const Home = ({ homepage }) => {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <title>{homepage.attributes.seo.metaTitle}</title>
-        <meta
-          name="description"
-          content={homepage.attributes.seo.metaDescription}
-        />
-      </Head>
-      <Normalize />
-      <h1>{homepage.attributes.hero.title}</h1>
-      <Badge text="Badge" />
-    </>
-  )
-}
-
-export async function getStaticProps() {
-  // Run API calls in parallel
-  const [homepageRes] = await Promise.all([
-    fetchAPI("/homepage", {
-      populate: {
-        hero: "*",
-        seo: { populate: "*" },
-      },
-    }),
-  ])
-
-  return {
-    props: {
-      homepage: homepageRes.data,
-    },
-    revalidate: 1,
-  }
-}
+const Home = () => (
+  <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <title>Fundacja Sterczące Uszy</title>
+    </Head>
+    <Normalize />
+    <h1>Sterczące Uszy</h1>
+    <Badge text="Badge" />
+  </>
+)
 
 export default Home
