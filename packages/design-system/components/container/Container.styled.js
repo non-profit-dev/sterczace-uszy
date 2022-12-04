8d7668c0842d71514bd0984b0a045bc18d557456
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-12-04 22:03:49 +0100

    feat: create Container/Section component

---
+++ packages/design-system/components/container/Container.styled.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled"
+
+const sizes = {
+  small: {
+    maxWidth: "390px",
+  },
+  medium: {
+    maxWidth: "768px",
+  },
+  large: {
+    maxWidth: "1216px",
+  },
+}
+
+export const Container = styled.div`
+  max-width: ${({ size }) => sizes[size].maxWidth};
+`
