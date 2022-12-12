b958546ec6ade9f30bef7883de5ba591e780e8e7
unknown <amanda.krempa1@gmail.com>
2022-12-12 18:26:40 +0100

    fix: merge main

--- packages/frontend/app/views/HomePage/HomePage.jsx
+++ packages/frontend/app/views/HomePage/HomePage.jsx
@@ -7,47 +7,47 @@ import * as Styled from "./HomePage.styled"
 const HomePage = () => {
   const theme = useTheme()
   return (
-      <Styled.Background>
-        <Styled.HomePageContainer>
-          <TextBanner
-            subtitle="FUNDACJA STERCZĄCE USZY"
-            heading="Strona w budowie"
-            description="Już za chwilę wracamy z nasza nową stroną. Zwierzaki jednak potrzebują pomocy cały czas. Wejdź na stronę ratujemyzwierzaki.pl i wesprzyj naszą fundację ❤"
-            size="large"
-            layout="center"
-            color={theme.colors.neutrals[100]}
-            button={
-              <Button
-                text="Przekaż wsparcie"
-                variant="fill"
-                color="primary"
-                href="https://www.facebook.com/sterczaceuszy"
-                size="medium"
-              />
-            }
-          />
-          <Styled.IconContainer>
-            <Icon
-              name="facebook"
-              size="large"
-              color={theme.colors.neutrals[100]}
+    <Styled.Background>
+      <Styled.HomePageContainer>
+        <TextBanner
+          subtitle="FUNDACJA STERCZĄCE USZY"
+          heading="Strona w budowie"
+          description="Już za chwilę wracamy z nasza nową stroną. Zwierzaki jednak potrzebują pomocy cały czas. Wejdź na stronę ratujemyzwierzaki.pl i wesprzyj naszą fundację ❤"
+          size="large"
+          layout="center"
+          color={theme.colors.neutrals[100]}
+          button={
+            <Button
+              text="Przekaż wsparcie"
+              variant="fill"
+              color="primary"
               href="https://www.facebook.com/sterczaceuszy"
+              size="medium"
             />
-            <Icon
-              name="instagram"
-              size="large"
-              color={theme.colors.neutrals[100]}
-              href="https://www.instagram.com/sterczaceuszy"
-            />
-            <Icon
-              name="mail"
-              size="large"
-              color={theme.colors.neutrals[100]}
-              href="mailto:fundacjasterczaceuszy@gmail.com"
-            />
-          </Styled.IconContainer>
-        </Styled.HomePageContainer>
-      </Styled.Background>
+          }
+        />
+        <Styled.IconContainer>
+          <Icon
+            name="facebook"
+            size="large"
+            color={theme.colors.neutrals[100]}
+            href="https://www.facebook.com/sterczaceuszy"
+          />
+          <Icon
+            name="instagram"
+            size="large"
+            color={theme.colors.neutrals[100]}
+            href="https://www.instagram.com/sterczaceuszy"
+          />
+          <Icon
+            name="mail"
+            size="large"
+            color={theme.colors.neutrals[100]}
+            href="mailto:fundacjasterczaceuszy@gmail.com"
+          />
+        </Styled.IconContainer>
+      </Styled.HomePageContainer>
+    </Styled.Background>
   )
 }
 
