c505a9a49949f63dd4e5f643c0f8f9c228fd7aa1
Amadriella <nconnect1789@gmail.com>
2022-12-13 12:35:16 +0100

    feat: add IconButton component with corresponding storybook and jest files

---
+++ packages/design-system/components/iconButton/IconButton.jsx
@@ -0,0 +1,29 @@
+import { string, oneOf, func, object } from "prop-types"
+
+import Icon, { icons } from "design-system/components/icon"
+import theme from "../../theme/theme"
+
+import * as Styled from "./IconButton.styled"
+
+const IconButton = ({ href, name, color, size, onClick }) => (
+  <Styled.Component as={href ? `a` : `button`} href={href} onClick={onClick}>
+    <Icon name={name} size={size} color={color} />
+  </Styled.Component>
+)
+
+IconButton.propTypes = {
+  href: string,
+  name: oneOf(Object.keys(icons)).isRequired,
+  color: string,
+  size: oneOf(["small", "medium", "large", "xlarge", "xxlarge"]),
+  onClick: func,
+}
+
+IconButton.defaultProps = {
+  href: null,
+  color: theme.colors.primary[500],
+  size: "medium",
+  onClick: null,
+}
+
+export default IconButton
---
+++ packages/design-system/components/iconButton/IconButton.styled.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+
+export const Component = styled.a`
+  display: flex;
+  cursor: pointer;
+  text-decoration: none;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+`
