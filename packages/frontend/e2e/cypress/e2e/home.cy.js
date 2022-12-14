678f466501ae574ca7fb79b13605f010a875a57d
Klaudia <78494852+im-klau@users.noreply.github.com>
2022-12-14 12:09:43 +0000

    chore: implement basic accessibility testing

--- packages/frontend/e2e/cypress/e2e/home.cy.js
+++ packages/frontend/e2e/cypress/e2e/home.cy.js
@@ -2,8 +2,14 @@ describe("Home Page", () => {
   it("successfully loads", () => {
     cy.visit("/");
   });
+  it(`should be accessible`, () => {
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+  it(`renders the main heading`, () => {
+    cy.get("h1").contains("Strona w budowie");
+  });
   it("find the link with href attribute", () => {
-    cy.visit("/");
     cy.get("span").contains("Przekaż wsparcie");
   });
 });
