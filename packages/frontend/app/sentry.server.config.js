a44a993d5275df3428defa5d53a3b001256e2afe
Klaudia <78494852+im-klau@users.noreply.github.com>
2023-01-10 14:00:59 +0000

    fix: remove sentry dsn

--- packages/frontend/app/sentry.server.config.js
+++ packages/frontend/app/sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://da808fdde393428ea358da2de7a5f209@o4504480359579648.ingest.sentry.io/4504480466927616',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
