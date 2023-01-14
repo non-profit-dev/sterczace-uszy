f47f160684386f089aeb2abf1ad8d590df0dfb66
imklau <klaudiazawodnik@gmail.com>
2023-01-14 15:42:02 +0100

    chore: include e2e dir in eslint

--- packages/frontend/e2e/cypress.config.js
+++ packages/frontend/e2e/cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     video: false,
   },
 });
