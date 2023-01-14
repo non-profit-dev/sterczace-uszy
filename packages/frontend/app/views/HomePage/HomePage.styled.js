9c5ae70d2ff7b7d5a4c6093b9652c07f1fff9ae5
imklau <klaudiazawodnik@gmail.com>
2023-01-14 13:26:15 +0100

    fix: rwd

--- packages/frontend/app/views/HomePage/HomePage.styled.js
+++ packages/frontend/app/views/HomePage/HomePage.styled.js
@@ -23,8 +23,8 @@ export const Container = styled.div`
 export const TextContainer = styled.div`
   position: relative;
   z-index: 1;
-  max-width: 580px;
-  padding: 48px 20px;
+  max-width: 620px;
+  padding: 48px 40px;
 
   ${({ theme }) => theme.breakpoints.sm} {
     padding-left: 16px;
@@ -36,7 +36,7 @@ export const Background = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
-  transform: translateX(30%);
+  transform: translateX(25%);
   width: 100%;
   height: 100%;
   max-height: 1000px;
@@ -49,6 +49,10 @@ export const Background = styled.div`
   ${({ theme }) => theme.breakpoints.lg} {
     max-height: 500px;
   }
+
+  ${({ theme }) => theme.breakpoints.sm} {
+    max-height: 50vh;
+  }
 `
 
 export const Img = styled.img`
@@ -56,6 +60,11 @@ export const Img = styled.img`
   height: 100%;
   object-fit: contain;
   background-color: ${({ theme }) => theme.colors.grey[600]};
+
+  ${({ theme }) => theme.breakpoints.sm} {
+    object-fit: cover;
+    object-position: top;
+  }
 `
 
 export const IconContainer = styled.div`
