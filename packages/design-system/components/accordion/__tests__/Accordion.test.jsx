eebba34de967532924711fe9eb13c5ae4fa830cb
EMILIA Buczek <emila1374@wp.pl>
2023-01-16 09:22:27 +0100

    ci: fixed test, added test for accordion

--- packages/design-system/components/accordion/__tests__/Accordion.test.jsx
+++ packages/design-system/components/accordion/__tests__/Accordion.test.jsx
@@ -2,11 +2,12 @@ import { render, screen } from "design-system/test-utils"
 
 import Accordion from "design-system/components/accordion"
 
-const color = "activeColor"
+const text = "Lorem ipsum"
 
-describe(`Banner`, () => {
-  it(`renders Button with default properties`, () => {
-    render(<Accordion color={color} />)
-    expect(screen.getByText(color)).toBeInTheDocument()
+describe(`Badge`, () => {
+  it(`renders with default properties`, () => {
+    render(<Accordion text={text} />)
+
+    expect(screen.getByText(text)).toBeInTheDocument()
   })
 })
