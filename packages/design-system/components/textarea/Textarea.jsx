549dde931c178585defbd6669580cf9b67317acc
Amanda <97829893+AmandaKr@users.noreply.github.com>
2023-01-17 20:03:38 +0100

    feat: create textarea component (#76)

---
+++ packages/design-system/components/textarea/Textarea.jsx
@@ -0,0 +1,67 @@
+import { bool, string } from "prop-types"
+import Icon from "design-system/components/icon"
+import Typography from "design-system/components/typography"
+import theme from "design-system/theme"
+
+import * as Styled from "./Textarea.styled"
+
+const Textarea = ({
+  label,
+  placeholder,
+  error,
+  message,
+  required,
+  disabled,
+  name,
+  id,
+}) => (
+  <Styled.Label>
+    <Typography variant="bodySmall">{label}</Typography>
+    <Styled.Container>
+      <Styled.Textarea
+        placeholder={placeholder}
+        error={error}
+        required={required}
+        disabled={disabled}
+        name={name}
+        id={id}
+      />
+      {error && (
+        <Styled.Icon error={error}>
+          <Icon name="error" size="medium" />
+        </Styled.Icon>
+      )}
+    </Styled.Container>
+    <Typography
+      variant="bodyTiny"
+      color={error ? theme.colors.error[100] : theme.colors.grey[500]}
+      required={required}
+      data-testid="message"
+    >
+      {required ? "Pole wymagane" : message}
+    </Typography>
+  </Styled.Label>
+)
+
+Textarea.propTypes = {
+  label: string.isRequired,
+  placeholder: string,
+  error: bool,
+  message: string,
+  required: bool,
+  disabled: bool,
+  name: string,
+  id: string,
+}
+
+Textarea.defaultProps = {
+  placeholder: "",
+  error: false,
+  message: "",
+  required: false,
+  disabled: false,
+  name: null,
+  id: null,
+}
+
+export default Textarea
