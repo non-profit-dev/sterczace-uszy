74dc1ae81acc947167587e96dde56d6994614014
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-01-21 10:22:53 +0100

    ci: updated styles, moved color and background to accordion and updated stories

--- packages/design-system/components/accordion/Accordion.styled.js
+++ packages/design-system/components/accordion/Accordion.styled.js
@@ -9,20 +9,9 @@ export const Accordion = styled.div`
   align-items: flex-start;
   justify-content: start;
   border-radius: 10px;
-`
-export const Title = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-left: auto;
-  width: 100%;
-  float: right;
-  text-decoration: none;
-  border-radius: 10px 10px 0px 0px;
-  padding: 20px 20px 0px 25px;
   box-shadow: 2px 2px 9px 0px rgba(0, 0, 0, 0.1);
-  ${({ theme }) => theme.typography.desktop.bodySmall};
-  background-color: ${({ color, theme }) => {
-    switch (color) {
+  background-color: ${({ activeColor, color, theme }) => {
+    switch ((activeColor, color)) {
       case "white":
         return theme.colors.neutrals[100]
       case "black":
@@ -50,49 +39,27 @@ export const Title = styled.div`
     }
   }};
 `
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  width: 100%;
+  text-decoration: none;
+  border-radius: 10px 10px 0px 0px;
+  padding: 20px 20px 0px 25px;
+  ${({ theme }) => theme.typography.desktop.bodySmall};
+`
 
 export const Content = styled.div`
   border-radius: 0px 0px 10px 10px;
-  width: auto;
   text-decoration: none;
   padding: 20px 20px 20px 20px;
   width: 100%;
-  box-shadow: 2px 2px 9px 0px rgba(0, 0, 0, 0.1);
-  background-color: ${({ color, theme }) => {
-    switch (color) {
-      case "white":
-        return theme.colors.neutrals[100]
-      case "black":
-        return theme.colors.grey[600]
-      case "primary":
-        return theme.colors.neutrals[100]
-      default:
-        return theme.colors.grey[600]
-    }
-  }};
-  color: ${({ color, theme }) => {
-    switch (color) {
-      case "white":
-        return theme.colors.grey[600]
-      case "black":
-        return theme.colors.neutrals[100]
-      default:
-        return theme.colors.grey[600]
-    }
-  }};
 `
 
 export const Icon = styled.div`
-  gap: 100px;
-  ${({ activeColor, color }) =>
-    color === "white" &&
-    activeColor === "primary" &&
-    css`
-      transform: rotate(180deg);
-    `}
-  ${({ activeColor, color }) =>
-    color === "black" &&
-    activeColor === "neutrals" &&
+  ${({ activeColor }) =>
+    activeColor &&
     css`
       transform: rotate(180deg);
     `}
