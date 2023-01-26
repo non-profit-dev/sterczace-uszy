0b46552ec02f3b0d2ca815078a5ddada8e3ae831
Amadriella <nconnect1789@gmail.com>
2023-01-26 10:23:17 +0100

    fix: textarea tests

--- packages/design-system/components/textarea/__tests__/Textarea.test.jsx
+++ packages/design-system/components/textarea/__tests__/Textarea.test.jsx
@@ -2,19 +2,19 @@ import userEvent from "@testing-library/user-event"
 
 import { render, screen } from "design-system/test-utils"
 
-import Textarea from "design-system/components/input"
+import Textarea from "design-system/components/textarea"
 
 const label = "Example label"
 const requiredMessage = "Pole wymagane"
 const message = "Test message"
 
 describe(`Textarea`, () => {
-  // it(`renders with default properties`, () => {
-  //   render(<Textarea label={label} />)
-  //   expect(screen.getByText(label)).toBeInTheDocument()
-  //   expect(screen.getByRole("textbox")).toBeInTheDocument()
-  //   expect(screen.getByRole("textbox")).toBeEnabled()
-  // })
+  it(`renders with default properties`, () => {
+    render(<Textarea label={label} />)
+    expect(screen.getByText(label)).toBeInTheDocument()
+    expect(screen.getByRole("textbox")).toBeInTheDocument()
+    expect(screen.getByRole("textbox")).toBeEnabled()
+  })
 
   it(`renders with custom properties`, () => {
     render(
@@ -66,7 +66,7 @@ describe(`Textarea`, () => {
   })
 
   it("has error icon when state is error", async () => {
-    render(<Textarea label={label} state="error" />)
+    render(<Textarea label={label} error />)
 
     const icon = screen.getByTitle("error")
     expect(icon).toBeVisible()
