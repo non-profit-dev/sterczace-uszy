d5ca609a867e982a88e9fad67587afbae7badcad
Amadriella <nconnect1789@gmail.com>
2023-02-06 12:20:41 +0100

    feat: add Decorative Card sketch

---
+++ packages/design-system/recipes/decorativeCard/DecorativeCard.jsx
@@ -0,0 +1,36 @@
+import { number, oneOf, string } from "prop-types"
+
+import Illustration from "design-system/components/illustration"
+import Count from "design-system/components/count"
+import Title from "design-system/components/title"
+
+import * as Styled from "./DecorativeCard.styled"
+
+const DecorativeCard = ({ titleText, illustrationName, count, size }) => (
+  <Styled.DecorativeCard>
+    <Styled.Content>
+      <Count count={count} size={size} />
+      <Styled.Illustration>
+        <Illustration name={illustrationName} />
+      </Styled.Illustration>
+    </Styled.Content>
+
+    <Title text={titleText} size="large" variant="text" />
+  </Styled.DecorativeCard>
+)
+export default DecorativeCard
+
+DecorativeCard.propTypes = {
+  /**
+   * The main heading of the card.
+   */
+  titleText: string.isRequired,
+  illustrationName: string,
+  count: number.isRequired,
+  size: oneOf(["small", "medium", "large", "xlarge", ""]),
+}
+
+DecorativeCard.defaultProps = {
+  illustrationName: "heartWithDogFace",
+  size: "large",
+}
---
+++ packages/design-system/recipes/decorativeCard/DecorativeCard.styled.js
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+
+export const DecorativeCard = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid;
+  padding: 2px;
+`
+export const Content = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`
+export const Illustration = styled.div`
+  position: relative;
+  top: -2px;
+  left: -20px;
+  border: 1px solid;
+  padding: 5px;
+`
