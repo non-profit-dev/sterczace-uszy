79ac98eb25edd794308d153a82c078b201259bf2
imklau <klaudiazawodnik@gmail.com>
2023-02-13 18:58:58 +0100

    feat(card): add responsive layout

--- packages/design-system/patterns/card/Card.jsx
+++ packages/design-system/patterns/card/Card.jsx
@@ -1,13 +1,16 @@
-import { string, oneOf, node, oneOfType, object } from "prop-types"
+import { string, oneOf, node, oneOfType, object, bool } from "prop-types"
 
 import Illustration from "design-system/components/illustration"
 import * as Styled from "./Card.styled"
 
 const Card = ({
   button,
+  hideButtonOnMobile,
   children,
   href,
   illustrationName,
+  hideIllustrationOnMobile,
+  mobileLayout,
   layout,
   title,
   bgColor,
@@ -22,7 +25,9 @@ const Card = ({
     href={href}
   >
     {illustrationName && (
-      <Styled.IllustrationContainer>
+      <Styled.IllustrationContainer
+        hideIllustrationOnMobile={hideIllustrationOnMobile}
+      >
         <Illustration name={illustrationName} />
       </Styled.IllustrationContainer>
     )}
@@ -33,14 +38,17 @@ const Card = ({
     )}
     <Styled.ContentContainer
       layout={layout}
+      mobileLayout={mobileLayout}
       paddingTop={!imageSrc && !illustrationName}
     >
-      <Styled.Wrapper layout={layout}>
+      <Styled.Wrapper layout={layout} mobileLayout={mobileLayout}>
         {title}
         {children}
       </Styled.Wrapper>
 
-      {button && button}
+      <Styled.ButtonWrapper hideButtonOnMobile={hideButtonOnMobile}>
+        {button && button}
+      </Styled.ButtonWrapper>
     </Styled.ContentContainer>
   </Styled.Card>
 )
@@ -52,28 +60,34 @@ Card.propTypes = {
   title: node.isRequired,
   bgColor: oneOfType([object, string]),
   button: node,
+  hideButtonOnMobile: bool,
   children: node,
   /**
    * The URL that the card should redirect to when clicked.
    */
   href: string,
   illustrationName: string,
+  hideIllustrationOnMobile: bool,
   imageAlt: string,
   imageHeight: string,
   imageSrc: string,
   layout: oneOf(["left", "center"]),
+  mobileLayout: oneOf(["left", "center"]),
 }
 
 Card.defaultProps = {
   bgColor: "null",
   button: null,
+  hideButtonOnMobile: false,
   children: null,
   href: null,
   illustrationName: null,
+  hideIllustrationOnMobile: false,
   imageAlt: null,
-  imageHeight: "auto",
+  imageHeight: null,
   imageSrc: null,
-  layout: "center",
+  layout: "left",
+  mobileLayout: "center",
 }
 
 export default Card
