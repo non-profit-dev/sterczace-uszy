aa795169eba47384fcf5f8b6c1c560e91aa3e0fe
Klaudia <78494852+im-klau@users.noreply.github.com>
2023-02-21 10:21:40 +0100

    feat: navigation (#88)
    Co-authored-by: imklau <klaudiazawodnik@gmail.com>

---
+++ packages/design-system/blocks/navigation/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    title: "Fundacja",
+    href: "/fundacja",
+  },
+  {
+    title: "Nasi podopieczni",
+    href: "/podopieczni",
+  },
+  {
+    title: "Adopcja",
+    href: "/adopcja",
+  },
+  {
+    title: "Jak pomóc",
+    href: "/jak-pomoc",
+  },
+  {
+    title: "Kontakt",
+    href: "/kontakt",
+  },
+  {
+    title: "Pliki do pobrania",
+    href: "/dokumenty",
+  },
+]
+
+export default data
