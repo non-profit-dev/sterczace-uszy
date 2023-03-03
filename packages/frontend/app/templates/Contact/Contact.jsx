7c92ea986f1ed70cda9d21704ce1fc7cc7b07da0
imklau <klaudiazawodnik@gmail.com>
2023-03-03 14:24:45 +0100

    feat: init form submit handler

--- packages/frontend/app/templates/Contact/Contact.jsx
+++ packages/frontend/app/templates/Contact/Contact.jsx
@@ -1,19 +1,34 @@
+import { useForm } from "@formspree/react"
+
 import Container from "design-system/components/container/Container"
-import * as Styled from "./Contact.styled"
+
 import Page from "../Page"
 import Form from "./components/Form"
 import ContactInfo from "./components/ContactInfo"
-// import Response from "./components/Response"
+import Response from "./components/Response"
 
-const Contact = () => (
-  <Page title="Kontakt">
-    <Container size="medium">
-      <Styled.ContactContainer>
-        <ContactInfo />
-        <Form />
-        {/* <Response /> */}
-      </Styled.ContactContainer>
-    </Container>
-  </Page>
-)
+import * as Styled from "./Contact.styled"
+
+const Contact = () => {
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM_ID, {
+    data: {
+      subject: "Kontakt - nowa wiadomość",
+    },
+  })
+
+  return (
+    <Page title="Kontakt">
+      <Container size="medium">
+        <Styled.ContactContainer>
+          <ContactInfo />
+          {state.succeeded ? (
+            <Response />
+          ) : (
+            <Form handleSubmit={handleSubmit} submitting={state.submitting} />
+          )}
+        </Styled.ContactContainer>
+      </Container>
+    </Page>
+  )
+}
 export default Contact
--- packages/frontend/app/templates/Contact/components/Form/Form.jsx
+++ packages/frontend/app/templates/Contact/components/Form/Form.jsx
@@ -1,35 +1,42 @@
+import { func, bool } from "prop-types"
 import { useTheme } from "@emotion/react"
+
 import Typography from "design-system/components/typography"
 import Input from "design-system/components/input"
 import Select from "design-system/components/select"
 import Textarea from "design-system/components/textarea"
 import Button from "design-system/components/button"
+
 import * as Styled from "./Form.styled"
 
-const Form = () => {
+const Form = ({ handleSubmit, submitting }) => {
   const theme = useTheme()
+
   return (
     <Styled.FormContainer>
       <Typography variant="h3" color={theme.colors.grey[600]}>
         Zostaw wiadomość
       </Typography>
-      <Styled.Form>
+      <Styled.Form onSubmit={handleSubmit}>
         <Styled.InputContainer>
           <Input
             label="Wpisz Twoje imię"
             placeholder="Twoje imię"
             type="text"
+            name="Imię"
             required
           />
           <Input
             label="Wpisz Twój e-mail"
             placeholder="kate.gregorczyk@gmail.com"
             type="email"
+            name="Mail"
             required
           />
         </Styled.InputContainer>
         <Select
           label="Temat"
+          name="Temat"
           options={[
             "adopcja",
             "dom tymczasowy",
@@ -45,14 +52,23 @@ const Form = () => {
           label="Twoja wiadomość"
           placeholder="Napisz dla nas wiadomość"
           required
+          name="Wiadomość"
           minLength={50}
         />
         <Styled.ButtonContainer>
-          <Button text="Wyślij" />
+          <Button
+            text={submitting ? "Wysyłanie" : "Wyślij"}
+            disabled={submitting}
+          />
         </Styled.ButtonContainer>
       </Styled.Form>
     </Styled.FormContainer>
   )
+}
+
+Form.propTypes = {
+  handleSubmit: func.isRequired,
+  submitting: bool.isRequired,
 }
 
 export default Form
