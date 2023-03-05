a08f6371338c0538c5b4f48266dc2081bc549768
imklau <klaudiazawodnik@gmail.com>
2023-03-05 19:11:31 +0100

    test(contact): intercept the form submit

--- packages/frontend/e2e/cypress/e2e/contact.cy.js
+++ packages/frontend/e2e/cypress/e2e/contact.cy.js
@@ -1,7 +1,3 @@
-// import React from "react"
-// import { mount } from "cypress/react18"
-// import Form from "frontend/templates/Contact/components/Form/Form"
-
 describe("Contact Page", () => {
   it("successfully loads", () => {
     cy.visit("/kontakt/")
@@ -20,10 +16,10 @@ describe("Contact Page", () => {
     cy.get("h2").contains("Zostaw wiadomość")
   })
 
-  // it(`sends the form after clicking "Wyślij" button`, () => {
-  it(`fills up the form`, () => {
-    // const handleSubmit = cy.stub().as("handleSubmitSpy")
-    // mount(<Form handleSubmit={handleSubmit} submitting />)
+  it(`sends the form after clicking submit button`, () => {
+    cy.intercept("POST", "https://formspree.io/f/*", { ok: true }).as(
+      "formSubmit"
+    )
 
     cy.get('input[name="Imię"]').type("test")
     cy.get('input[name="Mail"]').type("test@test")
@@ -32,7 +28,6 @@ describe("Contact Page", () => {
       "Hi, I'm sending test message, it has to be 30 characters long."
     )
 
-    // cy.get("button").contains("Wyślij").click()
-    // cy.get("@handleSubmitSpy").should("be.called")
+    cy.get("form").submit()
   })
 })
