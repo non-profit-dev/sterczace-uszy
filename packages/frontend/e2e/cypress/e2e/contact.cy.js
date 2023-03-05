57a8babe54d290d2e0a5bba5c1ee04025f991d8d
Paulina Grymulska <paulina.grymulska@gmail.com>
2023-03-05 10:06:47 +0100

    test: add filling the form

--- packages/frontend/e2e/cypress/e2e/contact.cy.js
+++ packages/frontend/e2e/cypress/e2e/contact.cy.js
@@ -1,3 +1,7 @@
+// import React from "react"
+// import { mount } from "cypress/react18"
+// import Form from "frontend/templates/Contact/components/Form/Form"
+
 describe("Contact Page", () => {
   it("successfully loads", () => {
     cy.visit("/kontakt/")
@@ -14,5 +18,21 @@ describe("Contact Page", () => {
 
   it(`renders the form heading`, () => {
     cy.get("h2").contains("Zostaw wiadomość")
+  })
+
+  // it(`sends the form after clicking "Wyślij" button`, () => {
+  it(`fills up the form`, () => {
+    // const handleSubmit = cy.stub().as("handleSubmitSpy")
+    // mount(<Form handleSubmit={handleSubmit} submitting />)
+
+    cy.get('input[name="Imię"]').type("test")
+    cy.get('input[name="Mail"]').type("test@test")
+    cy.get("select").select("dom-tymczasowy")
+    cy.get("textarea").type(
+      "Hi, I'm sending test message, it has to be 30 characters long."
+    )
+
+    // cy.get("button").contains("Wyślij").click()
+    // cy.get("@handleSubmitSpy").should("be.called")
   })
 })
