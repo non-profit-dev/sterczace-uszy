89f7d10c8b76acd45cbbad17c1a1542c43e36528
Amadriella <nconnect1789@gmail.com>
2023-03-11 12:22:59 +0100

    feat: add decorative card, ilustrations

--- packages/design-system/components/illustration/Illustration.jsx
+++ packages/design-system/components/illustration/Illustration.jsx
@@ -1,39 +1,95 @@
-import { oneOf, string } from "prop-types"
+import { oneOf } from "prop-types"
 
 import HeartWithDogFace from "design-system/tokens/illustrations/heartWithDogFace.svg"
+import HeartIllustration from "design-system/tokens/illustrations/HeartIllustration.svg"
+import DogIllustration from "design-system/tokens/illustrations/DogIllustration.svg"
+import HomeIllustration from "design-system/tokens/illustrations/HomeIllustration.svg"
 
 const sizes = {
   heartWithDogFace: {
-    width: 177,
-    height: 160,
+    small: {
+      width: 177,
+      height: 160,
+    },
+    medium: {
+      width: 177,
+      height: 160,
+    },
+    large: {
+      width: 177,
+      height: 160,
+    },
+  },
+  heart: {
+    small: {
+      width: 78,
+      height: 70,
+    },
+    medium: {
+      width: 103,
+      height: 92,
+    },
+    large: {
+      width: 121,
+      height: 109,
+    },
+  },
+  dog: {
+    small: {
+      width: 67,
+      height: 74,
+    },
+    medium: {
+      width: 93,
+      height: 99,
+    },
+    large: {
+      width: 110,
+      height: 116,
+    },
+  },
+  home: {
+    small: {
+      width: 59,
+      height: 68,
+    },
+    medium: {
+      width: 78,
+      height: 91,
+    },
+    large: {
+      width: 91,
+      height: 107,
+    },
   },
 }
 
 export const illustrations = {
   heartWithDogFace: HeartWithDogFace,
+  heart: HeartIllustration,
+  dog: DogIllustration,
+  home: HomeIllustration,
 }
 
-const Illustration = ({ name, className }) => {
+const Illustration = ({ name, size }) => {
   const IllustrationComponent = illustrations[name]
 
   return (
     <IllustrationComponent
-      width={sizes[name].width}
-      height={sizes[name].height}
+      width={sizes[name][size].width}
+      height={sizes[name][size].height}
       title={name}
       aria-hidden="true"
-      className={className}
     />
   )
 }
 
 Illustration.propTypes = {
   name: oneOf(Object.keys(illustrations)).isRequired,
-  className: string,
+  size: oneOf(["small", "medium", "large"]),
 }
-
 Illustration.defaultProps = {
-  className: null,
+  size: "large",
 }
 
 export default Illustration
