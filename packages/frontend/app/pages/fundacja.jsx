757ab539b943c88582e30479ec159529bff41e61
imklau <klaudiazawodnik@gmail.com>
2023-03-14 18:53:32 +0100

    feat(): create about page setup

---
+++ packages/frontend/app/pages/fundacja.jsx
@@ -0,0 +1,53 @@
+import AboutPage from "../templates/About"
+
+import {
+  supportingType,
+  animalsType,
+  adoptedAnimalsType,
+} from "../templates/HomePage/types"
+
+import client from "../lib/api"
+import {
+  GET_ADOPTED_ANIMALS,
+  GET_ANIMALS,
+  GET_SUPPORTING,
+} from "../lib/queries"
+
+const About = ({ animals, supporting, adoptedAnimals }) => (
+  <AboutPage
+    animals={animals}
+    supporting={supporting}
+    adoptedAnimals={adoptedAnimals}
+  />
+)
+
+export async function getStaticProps() {
+  const { data: animals } = await client.query({
+    query: GET_ANIMALS,
+  })
+
+  const { data: supporting } = await client.query({
+    query: GET_SUPPORTING,
+  })
+
+  const { data: adoptedAnimals } = await client.query({
+    query: GET_ADOPTED_ANIMALS,
+  })
+
+  return {
+    props: {
+      animals: animals.animalCollection,
+      adoptedAnimals: adoptedAnimals.animalCollection,
+      supporting: supporting.supportingCollection,
+      fallback: true,
+    },
+  }
+}
+
+About.propTypes = {
+  animals: animalsType.isRequired,
+  supporting: supportingType.isRequired,
+  adoptedAnimals: adoptedAnimalsType.isRequired,
+}
+
+export default About
---
+++ packages/frontend/app/templates/About/About.jsx
@@ -0,0 +1,57 @@
+import Container from "design-system/components/container"
+import Banner from "design-system/components/banner"
+import Button from "design-system/components/button"
+import Typography from "design-system/components/typography"
+
+import Navigation from "design-system/blocks/navigation"
+import Footer from "design-system/blocks/footer"
+
+import Page from "../Page"
+
+import { supportingType, animalsType, adoptedAnimalsType } from "./types"
+
+import * as Styled from "./About.styled"
+
+const About = ({ animals, supporting, adoptedAnimals }) => (
+  <Page>
+    <Styled.Banner>
+      <Banner
+        heading="Nasi podopieczni czekają na Twoje wsparcie! Chcesz nam pomóc? Zajrzyj"
+        button={
+          <Button
+            text="tutaj"
+            href="/jak-pomoc"
+            variant="textLine"
+            size="small"
+          />
+        }
+      />
+    </Styled.Banner>
+
+    <Navigation />
+
+    <Styled.Main>
+      <Container>
+        <Typography variant="h4" as="h3">
+          ♥️ {supporting.total} Pomocnych serc
+        </Typography>
+        <Typography variant="h4" as="h3">
+          🏡 {adoptedAnimals.total} Znalezionych domów
+        </Typography>
+        <Typography variant="h4" as="h3">
+          🏡 {animals.total + adoptedAnimals.total} Uratowanych zwierząt
+        </Typography>
+      </Container>
+    </Styled.Main>
+
+    <Footer />
+  </Page>
+)
+
+About.propTypes = {
+  animals: animalsType.isRequired,
+  supporting: supportingType.isRequired,
+  adoptedAnimals: adoptedAnimalsType.isRequired,
+}
+
+export default About
