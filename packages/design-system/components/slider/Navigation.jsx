23a7f7efa7080677c6bdadad6b44c92b81b04853
Amanda <97829893+AmandaKr@users.noreply.github.com>
2023-03-19 18:51:06 +0100

    feat: slider (#106)
    Co-authored-by: imklau <klaudiazawodnik@gmail.com>
    Co-authored-by: Klaudia <78494852+im-klau@users.noreply.github.com>

---
+++ packages/design-system/components/slider/Navigation.jsx
@@ -0,0 +1,48 @@
+import { useTheme } from "@emotion/react"
+import { useSwiper } from "swiper/react"
+
+import Icon from "design-system/components/icon"
+
+import * as Styled from "./Slider.styled"
+
+const Navigation = () => {
+  const slider = useSwiper()
+  const theme = useTheme()
+
+  return (
+    <>
+      <Styled.Arrow
+        direction="prev"
+        onClick={() => {
+          slider.slidePrev()
+        }}
+      >
+        <Icon
+          name="chevronLeft"
+          color={
+            slider.isBeginning
+              ? theme.colors.grey[300]
+              : theme.colors.primary[400]
+          }
+          size="large"
+        />
+      </Styled.Arrow>
+      <Styled.Arrow
+        direction="next"
+        onClick={() => {
+          slider.slideNext()
+        }}
+      >
+        <Icon
+          name="chevronRight"
+          color={
+            slider.isEnd ? theme.colors.grey[300] : theme.colors.primary[400]
+          }
+          size="large"
+        />
+      </Styled.Arrow>
+    </>
+  )
+}
+
+export default Navigation
