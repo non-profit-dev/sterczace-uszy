1ed045d1d3c3a75518afdeaf277bf3c0fe6527cb
imklau <klaudiazawodnik@gmail.com>
2023-03-21 13:14:38 +0100

    fix(home): move button outside of textbanner

--- packages/frontend/app/templates/HomePage/components/Actions/Actions.styled.js
+++ packages/frontend/app/templates/HomePage/components/Actions/Actions.styled.js
@@ -3,11 +3,13 @@ import styled from "@emotion/styled"
 export const Content = styled.div`
   padding-bottom: 120px;
 `
+
 export const BannerContent = styled.div`
   display: flex;
   flex-direction: column;
   gap: 60px;
   align-items: center;
+
   ${({ theme }) => theme.breakpoints.tablet} {
     text-align: left;
     align-items: flex-start;
@@ -25,5 +27,17 @@ export const CardsContainer = styled.div`
   ${({ theme }) => theme.breakpoints.tabletLg} {
     grid-template-columns: 1fr;
     gap: 0;
+  }
+`
+
+export const ButtonWrapper = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${({ theme }) => theme.breakpoints.tablet} {
+    margin-top: 0;
+    align-items: flex-start;
   }
 `
