efd5757168a9c7c1b30ad3e3bf6595706c16e7ea
imklau <klaudiazawodnik@gmail.com>
2023-03-23 12:51:11 +0100

    feat(tabs): add transparent variant

--- packages/design-system/patterns/tabs/Tabs.jsx
+++ packages/design-system/patterns/tabs/Tabs.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { string, arrayOf, shape, oneOfType, node } from "prop-types"
+import { string, arrayOf, shape, oneOfType, node, bool } from "prop-types"
 
 import Typography from "design-system/components/typography"
 import Title from "design-system/components/title"
@@ -12,21 +12,22 @@ function convertToSlug(Text) {
     .replace(/[^\w-]+/g, "")
 }
 
-const Tabs = ({ data, id, className }) => {
+const Tabs = ({ data, id, transparentContent, className }) => {
   const [activeIndex, setActiveIndex] = useState(0)
 
   return (
     <Styled.Wrapper className={className}>
-      <Styled.Tabs role="tablist">
+      <Styled.Tabs role="tablist" transparentContent={transparentContent}>
         {data.map(({ tab }, index) => (
           <Styled.Tab
             key={tab}
+            role="tab"
             active={index === activeIndex}
             aria-selected={index === activeIndex}
             aria-controls={`${convertToSlug(id)}-${convertToSlug(tab)}`}
             id={convertToSlug(tab)}
-            role="tab"
             onClick={() => setActiveIndex(index)}
+            borderBottom={transparentContent || activeIndex !== index}
           >
             {activeIndex === index ? (
               <Title text={tab} textSize="h6" variant="textLine" />
@@ -45,6 +46,7 @@ const Tabs = ({ data, id, className }) => {
           tabindex="0"
           aria-labelledby={convertToSlug(tab)}
           hidden={index !== activeIndex}
+          transparentContent={transparentContent}
         >
           {content}
         </Styled.Content>
@@ -61,11 +63,13 @@ Tabs.propTypes = {
     })
   ).isRequired,
   id: string.isRequired,
+  transparentContent: bool,
   className: string,
 }
 
 Tabs.defaultProps = {
   className: null,
+  transparentContent: false,
 }
 
 export default Tabs
--- packages/design-system/patterns/tabs/Tabs.styled.js
+++ packages/design-system/patterns/tabs/Tabs.styled.js
@@ -1,23 +1,26 @@
 import styled from "@emotion/styled"
 
-export const Wrapper = styled.div`
-  // border: 1px solid black;
-`
+export const Wrapper = styled.div``
 
 export const Tabs = styled.div`
   display: flex;
   width: 100%;
+  filter: ${({ transparentContent }) =>
+    transparentContent
+      ? "drop-shadow(2px 3px 9px rgba(0, 0, 0, 0.1))"
+      : "none"};
 `
 
 export const Content = styled.div`
-  padding: 32px;
-  background: ${({ theme }) => theme.colors.complementary[100]};
-  border: 1px solid ${({ theme }) => theme.colors.gray[300]};
+  padding: ${({ transparentContent }) =>
+    transparentContent ? "32px 32px 32px 8px" : "32px"};
+  background: ${({ theme, transparentContent }) =>
+    transparentContent ? "transparent" : theme.colors.complementary[100]};
+  border: 1px solid
+    ${({ theme, transparentContent }) =>
+      transparentContent ? "transparent" : theme.colors.gray[300]};
   border-top: transparent;
   border-radius: 0 0 10px 10px;
-
-  ${({ theme }) => theme.breakpoints.tablet} {
-  }
 `
 
 export const Tab = styled.button`
@@ -28,7 +31,8 @@ export const Tab = styled.button`
     active ? theme.colors.complementary[100] : theme.colors.gray[100]};
   border: 1px solid ${({ theme }) => theme.colors.gray[300]};
   border-bottom: 1px solid
-    ${({ theme, active }) => (active ? "transparent" : theme.colors.gray[300])};
+    ${({ borderBottom, theme }) =>
+      borderBottom ? theme.colors.gray[300] : "transparent"};
 
   &:first-of-type {
     border-radius: 10px 0 0 0;
