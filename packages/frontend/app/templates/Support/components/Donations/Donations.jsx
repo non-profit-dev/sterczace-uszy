d075cf7503e1b0106d547ef38779b53b0e82e1ac
imklau <klaudiazawodnik@gmail.com>
2023-03-25 12:50:33 +0100

    feat: donations section

--- packages/frontend/app/templates/Support/components/Donations/Donations.jsx
+++ packages/frontend/app/templates/Support/components/Donations/Donations.jsx
@@ -1,4 +1,6 @@
 import Container from "design-system/components/container"
+import Title from "design-system/components/title"
+import Typography from "design-system/components/typography"
 
 import TextBanner from "design-system/patterns/textBanner"
 
@@ -7,14 +9,27 @@ import * as Styled from "./Donations.styled"
 const Donations = () => (
   <Container as="section" id="wplaty">
     <Styled.Content>
-      <TextBanner heading="Pomóż nam pomagać" subtitle="Wpłaty" size="medium">
-        Nasze potrzeby nigdy nie maleją. Każdego dnia mierzymy się z ogromnymi
-        wydatkami, aby zapewnić naszym podopiecznym najlepsze warunki. Możesz
-        nam pomóc przekazując wsparcie finansowe. Wybierz najbardziej dogodną
-        dla Ciebie opcję - jednorazowa wpłata lub zlecenie stałe. Niezależnie od
-        tego, na co się zdecydujesz, będziesz mieć wpływ na życie naszych
-        podopiecznych. Dziękujemy za każdą, nawet najmniejszą wpłatę.
-      </TextBanner>
+      <div>
+        <TextBanner heading="Pomóż nam pomagać" subtitle="Wpłaty" size="medium">
+          Nasze potrzeby nigdy nie maleją. Każdego dnia mierzymy się z ogromnymi
+          wydatkami, aby zapewnić naszym podopiecznym najlepsze warunki. Możesz
+          nam pomóc przekazując wsparcie finansowe. Wybierz najbardziej dogodną
+          dla Ciebie opcję - jednorazowa wpłata lub zlecenie stałe. Niezależnie
+          od tego, na co się zdecydujesz, będziesz mieć wpływ na życie naszych
+          podopiecznych. Dziękujemy za każdą, nawet najmniejszą wpłatę.
+        </TextBanner>
+
+        <Styled.Box>
+          <Title text="Przelew jednorazowy lub zlecenie stałe" variant="text" />
+          <Typography variant="bodyTitle">
+            Fundacja Sterczące Uszy ul. Adres 1, 91-666 Poznań NIP 5648753 Tytuł
+            przelewu: Wsparcie dla Fundacji
+          </Typography>
+          <Typography variant="h5">41 1870 1045 2078 1072 8639 0001</Typography>
+        </Styled.Box>
+      </div>
+
+      <Styled.Image src="/support/donations-image.png" />
     </Styled.Content>
   </Container>
 )
--- packages/frontend/app/templates/Support/components/Donations/Donations.styled.js
+++ packages/frontend/app/templates/Support/components/Donations/Donations.styled.js
@@ -1,6 +1,25 @@
 import styled from "@emotion/styled"
 
+import ImageComponent from "design-system/components/image"
+
 export const Content = styled.div`
+  display: grid;
+  gap: 60px;
+  grid-template-columns: 2fr 1fr;
+
   ${({ theme }) => theme.breakpoints.desktopSm} {
+  }
+`
+
+export const Box = styled.div`
+  max-width: 460px;
+  margin-top: 40px;
+  padding: 20px;
+  border: 1px dashed ${({ theme }) => theme.colors.primary[500]};
+`
+
+export const Image = styled(ImageComponent)`
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    display: none;
   }
 `
