b8c112c411dc70b592419d00ab4f1664a9149724
unknown <amanda.krempa1@gmail.com>
2023-03-27 18:14:28 +0200

    feat: add contact banner

--- packages/frontend/app/templates/Adoption/Adoption.jsx
+++ packages/frontend/app/templates/Adoption/Adoption.jsx
@@ -2,6 +2,7 @@ import Container from "design-system/components/container"
 import Banner from "design-system/components/banner"
 import Button from "design-system/components/button"
 import Typography from "design-system/components/typography"
+import CtaBanner from "design-system/patterns/ctaBanner"
 import Navigation from "design-system/blocks/navigation"
 import Footer from "design-system/blocks/footer"
 
@@ -13,7 +14,7 @@ import * as Styled from "./Adoption.styled"
 
 const Adoption = ({ animals }) => (
   <Page title="Nasi podopieczni">
-    <Styled.Banner>
+    <Banner>
       <Banner
         heading="Nasi podopieczni czekają na Twoje wsparcie! Chcesz nam pomóc? Zajrzyj"
         button={
@@ -25,7 +26,7 @@ const Adoption = ({ animals }) => (
           />
         }
       />
-    </Styled.Banner>
+    </Banner>
 
     <Navigation />
 
@@ -35,6 +36,12 @@ const Adoption = ({ animals }) => (
           ♥️ {animals.total} Pomocnych serc
         </Typography>
       </Container>
+      <CtaBanner
+        buttonHref="/kontakt"
+        buttonText="Skontaktuj się z nami"
+        heading="Masz pytania lub wątpliwości?"
+        mobileLayout="left"
+      />
     </Styled.Main>
 
     <Footer />
