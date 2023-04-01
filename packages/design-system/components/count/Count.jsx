efd51fe7b4f0d12a727c260c228489d86c7628f7
unknown <amanda.krempa1@gmail.com>
2023-04-01 16:23:41 +0200

    feat: add sizes to count component

--- packages/design-system/components/count/Count.jsx
+++ packages/design-system/components/count/Count.jsx
@@ -2,8 +2,8 @@ import { oneOf, number, string } from "prop-types"
 
 import * as Styled from "./Count.styled"
 
-const Count = ({ count, className, variant }) => (
-  <Styled.Count variant={variant} className={className}>
+const Count = ({ count, className, variant, size }) => (
+  <Styled.Count variant={variant} className={className} size={size}>
     {count}
   </Styled.Count>
 )
@@ -14,11 +14,13 @@ Count.propTypes = {
   count: number.isRequired,
   className: string,
   variant: oneOf(["title", "decorative"]),
+  size: oneOf(["xsmall", "small", "medium", "large"]),
 }
 
 Count.defaultProps = {
   className: null,
   variant: "title",
+  size: "medium",
 }
 
 export default Count
--- packages/design-system/components/count/Count.styled.js
+++ packages/design-system/components/count/Count.styled.js
@@ -1,23 +1,48 @@
 import styled from "@emotion/styled"
 
-export const Count = styled.p`
+const sizes = {
+  xsmall: {
+    desktop: { fontSize: "48px" },
+    tablet: { fontSize: "44px" },
+    mobile: { fontSize: "40px" },
+  },
+  small: {
+    desktop: { fontSize: "72px" },
+    tablet: { fontSize: "56px" },
+    mobile: { fontSize: "40px" },
+  },
+  medium: {
+    desktop: { fontSize: "110px" },
+    tablet: { fontSize: "90px" },
+    mobile: { fontSize: "60px" },
+  },
+  large: {
+    desktop: { fontSize: "140px" },
+    tablet: { fontSize: "100px" },
+    mobile: { fontSize: "70px" },
+  },
+}
+
+export const Count = styled.p(
+  ({ size, variant, theme }) => `
+ font-size : ${sizes[size].desktop.fontSize};
+
+  ${theme.breakpoints.tabletLg} {
+    font-size: ${sizes[size].tablet.fontSize};
+  }
+
+  ${theme.breakpoints.mobileLg} {
+    font-size: ${sizes[size].mobile.fontSize};
+  }
+
   text-align: center;
   display: inline-flex;
-  align-items: ${({ variant }) =>
-    variant === "title" ? `center` : `flex-end`};
+  align-items: ${variant === "title" ? `center` : `flex-end`};
   font-weight: 900;
-  font-size: ${({ variant }) => (variant === "title" ? `48px` : `110px`)};
-  line-height: 81px;
+  line-height: 74%;
   font-family: "Poppins";
-  color: ${({ theme }) => theme.colors.primary[500]};
+  color: ${theme.colors.primary[500]};
   margin: 0;
   padding: 0;
-  ${({ theme }) => theme.breakpoints.tablet} {
-    font-size: ${({ variant }) => (variant === "title" ? `48px` : `94px`)};
-    line-height: 70px;
-  }
-  ${({ theme }) => theme.breakpoints.mobile} {
-    font-size: ${({ variant }) => (variant === "title" ? `48px` : `70px`)};
-    line-height: 52px;
-  }
 `
+)
