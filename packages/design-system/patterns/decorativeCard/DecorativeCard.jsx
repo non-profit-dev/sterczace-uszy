397c7da73f13afceb6eb1d323d079bdbfb725310
unknown <amanda.krempa1@gmail.com>
2023-04-02 14:30:54 +0200

    fix: update decorativeCard to new count sizing

--- packages/design-system/patterns/decorativeCard/DecorativeCard.jsx
+++ packages/design-system/patterns/decorativeCard/DecorativeCard.jsx
@@ -9,7 +9,7 @@ import Typography from "../../components/typography/Typography"
 const DecorativeCard = ({ title, illustrationName, count }) => (
   <Styled.DecorativeCard>
     <Styled.Content>
-      <Count count={count} variant="decorative" />
+      <Count count={count} size="medium" />
       <Styled.Illustration>
         <Illustration name={illustrationName} size="large" />
       </Styled.Illustration>
