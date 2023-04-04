be28541cb2109b7d04ced8af076feb37e50b7bc7
imklau <klaudiazawodnik@gmail.com>
2023-04-04 14:17:18 +0200

    fix(animals): remove duplicated container

--- packages/frontend/app/templates/Animals/components/Animals/Animals.jsx
+++ packages/frontend/app/templates/Animals/components/Animals/Animals.jsx
@@ -1,7 +1,6 @@
 import { shape, arrayOf, string } from "prop-types"
 
 import Button from "design-system/components/button"
-import Container from "design-system/components/container"
 
 import TextBanner from "design-system/patterns/textBanner"
 import AnimalCard from "design-system/patterns/animalCard"
@@ -10,39 +9,37 @@ import * as Styled from "./Animals.styled"
 
 const Animals = ({ data }) => (
   <Styled.Wrapper>
-    <Container>
-      <Styled.ContentContainer>
-        <TextBanner
-          heading="Czekają na nowy dom"
-          subtitle="Nasi podopieczni"
-          tabletLayout="left"
-          mobileLayout="left"
-        >
-          Każdy z nich jest wyjątkowy. Starszaki i szczeniaki, duże i małe,
-          lubiące kanapę lub długie spacery. Potrzebują jedynie miłości.
-          Sprawdź, któremu odmienisz życie?
-        </TextBanner>
-      </Styled.ContentContainer>
+    <Styled.ContentContainer>
+      <TextBanner
+        heading="Czekają na nowy dom"
+        subtitle="Nasi podopieczni"
+        tabletLayout="left"
+        mobileLayout="left"
+      >
+        Każdy z nich jest wyjątkowy. Starszaki i szczeniaki, duże i małe,
+        lubiące kanapę lub długie spacery. Potrzebują jedynie miłości. Sprawdź,
+        któremu odmienisz życie?
+      </TextBanner>
+    </Styled.ContentContainer>
 
-      <Styled.CardsWrapper>
-        {data.map(({ name, age, gender, excerpt, thumbnail, slug }) => (
-          <Styled.CardWrapper key={name}>
-            <AnimalCard
-              key={name}
-              name={name}
-              age={age}
-              gender={gender}
-              excerpt={excerpt}
-              slug={slug}
-              image={thumbnail.url}
-            />
-          </Styled.CardWrapper>
-        ))}
-      </Styled.CardsWrapper>
-      <Styled.ButtonWrapper>
-        <Button text="Sprawdź zasady adopcji" href="/adopcja" size="large" />
-      </Styled.ButtonWrapper>
-    </Container>
+    <Styled.CardsWrapper>
+      {data.map(({ name, age, gender, excerpt, thumbnail, slug }) => (
+        <Styled.CardWrapper key={name}>
+          <AnimalCard
+            key={name}
+            name={name}
+            age={age}
+            gender={gender}
+            excerpt={excerpt}
+            slug={slug}
+            image={thumbnail.url}
+          />
+        </Styled.CardWrapper>
+      ))}
+    </Styled.CardsWrapper>
+    <Styled.ButtonWrapper>
+      <Button text="Sprawdź zasady adopcji" href="/adopcja" size="large" />
+    </Styled.ButtonWrapper>
   </Styled.Wrapper>
 )
 
