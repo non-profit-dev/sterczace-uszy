7a66af1af1aa9050f552a03c427b66ad11fc5818
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-05-19 11:13:08 +0200

    fix: introduce fix for the missing map, corrected missing maps (#143)
    Co-authored-by: Klaudia <78494852+im-klau@users.noreply.github.com>
    Co-authored-by: imklau <klaudiazawodnik@gmail.com>

--- packages/frontend/app/templates/Contact/components/ContactInfo/ContactInfo.jsx
+++ packages/frontend/app/templates/Contact/components/ContactInfo/ContactInfo.jsx
@@ -7,6 +7,24 @@ import Contact from "frontend/public/contact/Contact.svg"
 import globalData from "shared/data"
 import * as Styled from "./ContactInfo.styled"
 
+const listData = [
+  {
+    iconName: "phone",
+    text: `${globalData.contact.phoneFirst.text} - sms`,
+    link: globalData.contact.phoneFirst.link,
+  },
+  {
+    iconName: "phone",
+    text: `${globalData.contact.phoneSecond.text} - sms`,
+    link: globalData.contact.phoneSecond.link,
+  },
+  {
+    iconName: "mail",
+    text: globalData.contact.mail.text,
+    link: globalData.contact.mail.link,
+  },
+]
+
 const ContactInfo = () => {
   const theme = useTheme()
   return (
@@ -16,33 +34,22 @@ const ContactInfo = () => {
       </Typography>
       <Contact />
       <List gap={8}>
-        <ListItem iconName="phone" variant="gray" iconSize="small">
-          <Button
-            variant="text"
-            text={`${globalData.contact.phoneFirst.text} - sms`}
-            size="small"
-            color="black"
-            href={globalData.contact.phoneFirst.link}
-          />
-        </ListItem>
-        <ListItem iconName="phone" variant="gray" iconSize="small">
-          <Button
-            variant="text"
-            text={`${globalData.contact.phoneSecond.text} - sms`}
-            size="small"
-            color="black"
-            href={globalData.contact.phoneSecond.link}
-          />
-        </ListItem>
-        <ListItem iconName="mail" variant="gray" iconSize="small">
-          <Button
-            variant="text"
-            text={globalData.contact.mail.text}
-            size="small"
-            color="black"
-            href={globalData.contact.mail.link}
-          />
-        </ListItem>
+        {listData.map(({ text, iconName, link }) => (
+          <ListItem
+            key={text}
+            iconName={iconName}
+            variant="gray"
+            iconSize="small"
+          >
+            <Button
+              variant="text"
+              text={text}
+              size="small"
+              color="black"
+              href={link}
+            />
+          </ListItem>
+        ))}
       </List>
     </Styled.ContactInfoContainer>
   )
