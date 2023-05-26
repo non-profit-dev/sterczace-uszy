ae8fb81dcd276b94b77e226ae9f29ee0a18dd1f0
amand <amanda.krempa1@gmail.com>
2023-05-26 15:51:24 +0200

    fix: restore select code

--- packages/design-system/components/select/Select.jsx
+++ packages/design-system/components/select/Select.jsx
@@ -12,33 +12,39 @@ const Select = ({
   id,
   required,
   defaultValue,
-}) => (
-  // const convertToValue = (option) => option.replaceAll(" ", "-").toLowerCase()
+}) => {
+  const convertToValue = (option) => option.replaceAll(" ", "-").toLowerCase()
 
-  <Styled.Label>
-    <Typography variant="bodySmall">{label}</Typography>
-    <Styled.Container>
-      <Styled.Select
-        disabled={disabled}
-        name={name}
-        id={id}
-        required={required}
-      >
-        <>
-          {defaultValue && <option value={defaultValue}>{defaultValue}</option>}
-          {options.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </>
-      </Styled.Select>
-      <Styled.Icon>
-        <Icon name="chevronDown" size="medium" />
-      </Styled.Icon>
-    </Styled.Container>
-  </Styled.Label>
-)
+  return (
+    <Styled.Label>
+      <Typography variant="bodySmall">{label}</Typography>
+      <Styled.Container>
+        <Styled.Select
+          disabled={disabled}
+          name={name}
+          id={id}
+          required={required}
+        >
+          <>
+            {defaultValue && (
+              <option value={convertToValue(defaultValue)}>
+                {defaultValue}
+              </option>
+            )}
+            {options.map((option) => (
+              <option key={option} value={convertToValue(option)}>
+                {option}
+              </option>
+            ))}
+          </>
+        </Styled.Select>
+        <Styled.Icon>
+          <Icon name="chevronDown" size="medium" />
+        </Styled.Icon>
+      </Styled.Container>
+    </Styled.Label>
+  )
+}
 
 Select.propTypes = {
   label: string.isRequired,
