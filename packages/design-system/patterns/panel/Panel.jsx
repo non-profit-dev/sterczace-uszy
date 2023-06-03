2fb335dad24bc42ab2031d0005a23aec43bfd32d
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-06-03 15:00:51 +0200

    fix: added copy on click functionality, added function, so the icon copies bank account

--- packages/design-system/patterns/panel/Panel.jsx
+++ packages/design-system/patterns/panel/Panel.jsx
@@ -1,22 +1,35 @@
 import { string, node } from "prop-types"
 import IconButton from "design-system/components/iconButton"
 import Typography from "design-system/components/typography"
+import { useState } from "react"
 import * as Styled from "./Panel.styled"
 
-const Panel = ({ className, title, accountNumber, children }) => (
-  <Styled.PanelContent className={className}>
-    {title}
-    <Styled.Content>{children}</Styled.Content>
-    <Typography variant="h6">
-      {accountNumber}
-      {accountNumber && <IconButton name="copy" />}
-    </Typography>
-  </Styled.PanelContent>
-)
+const Panel = ({ className, title, accountNumber, children }) => {
+  const [isCopied, setIsCopied] = useState(false)
+  const handleCopy = () => {
+    navigator.clipboard.writeText(accountNumber)
+    setIsCopied((prevIsCopied) => !prevIsCopied)
+  }
+  return (
+    <Styled.PanelContent className={className}>
+      {title}
+      <Styled.Content>{children}</Styled.Content>
+      <Typography variant="h6">
+        {accountNumber}
+        {accountNumber && (
+          <Styled.IconButton>
+            <IconButton name="copy" size="medium" as="a" onClick={handleCopy} />
+          </Styled.IconButton>
+        )}
+        {isCopied && <Typography variant="bodyTiny">Skopiowano</Typography>}
+      </Typography>
+    </Styled.PanelContent>
+  )
+}
 
 Panel.propTypes = {
   title: node.isRequired,
-  accountNumber: node,
+  accountNumber: string,
   className: string,
   children: node,
 }
--- packages/design-system/patterns/panel/Panel.styled.js
+++ packages/design-system/patterns/panel/Panel.styled.js
@@ -1,15 +1,23 @@
 import styled from "@emotion/styled"
 
 export const PanelContent = styled.div`
+  position: relative;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  text-align: left;
   padding: 20px;
   border: 1px dashed ${({ theme }) => theme.colors.primary[400]};
 `
 
 export const Content = styled.div`
-  padding-top: 20px;
-  padding-bottom: 10px;
-  max-width: 90%;
+  float: left;
+  margin-bottom: 20px;
+  width: 80%;
+`
+
+export const IconButton = styled.div`
+  float: right;
+  margin-right: 20px;
 `
