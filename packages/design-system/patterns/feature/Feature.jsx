e21a45a7523bdac01e4c847209d75937fc5a4751
imklau <klaudiazawodnik@gmail.com>
2023-06-11 12:35:54 +0200

    feat(feature): add count support

--- packages/design-system/patterns/feature/Feature.jsx
+++ packages/design-system/patterns/feature/Feature.jsx
@@ -1,18 +1,20 @@
-import { string, node } from "prop-types"
+import { string, node, number, oneOf } from "prop-types"
 import { useTheme } from "@emotion/react"
 
 import Icon from "design-system/components/icon"
 import Typography from "design-system/components/typography"
+import Count from "design-system/components/count"
 
 import * as Styled from "./Feature.styled"
 
-const Feature = ({ heading, children, icon, className }) => {
+const Feature = ({ heading, children, icon, count, color, className }) => {
   const theme = useTheme()
 
   return (
     <Styled.Feature className={className}>
-      <Styled.Icon>
-        <Icon name={icon} color={theme.colors.neutrals[100]} />
+      <Styled.Icon color={color}>
+        {icon && <Icon name={icon} color={theme.colors.neutrals[100]} />}
+        {count && <Count count={count} size="xsmall" color="white" />}
       </Styled.Icon>
 
       <Styled.Content>
@@ -27,13 +29,18 @@ const Feature = ({ heading, children, icon, className }) => {
 
 Feature.propTypes = {
   heading: string.isRequired,
-  icon: string.isRequired,
+  icon: string,
+  count: number,
+  color: oneOf(["primary", "gray"]),
   children: node.isRequired,
   className: string,
 }
 
 Feature.defaultProps = {
   className: null,
+  color: "primary",
+  icon: null,
+  count: null,
 }
 
 export default Feature
