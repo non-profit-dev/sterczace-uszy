5e33d03ab9a1755dfeef264294191369784422a8
Klaudia <78494852+im-klau@users.noreply.github.com>
2023-07-03 18:39:41 +0200

    Update packages/frontend/app/templates/Support/components/Fanimani/Fanimani.jsx
    Co-authored-by: Amanda <97829893+AmandaKr@users.noreply.github.com>

--- packages/frontend/app/templates/Support/components/Fanimani/Fanimani.jsx
+++ packages/frontend/app/templates/Support/components/Fanimani/Fanimani.jsx
@@ -43,7 +43,7 @@ const steps = [
   {
     title: "Zainstaluj przypominajkę na komputerze",
     description:
-      "Jeżeli cześciej robisz zakupy na komputerze niż telefonie możesz zainstalować sobie przypominajkę, aby nigdy nie zapomnieć o pomaganiu. Przejdź na stronę  Fanimani i pobierz rozszerzenie",
+      "Jeżeli cześciej robisz zakupy na komputerze niż telefonie możesz zainstalować sobie przypominajkę, aby nigdy nie zapomnieć o pomaganiu. Przejdź na stronę Fanimani i pobierz rozszerzenie",
     illustration: `computer`,
     button: (
       <Button
