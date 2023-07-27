cd81c00eb72a88f932a7ca1b9b6b14606c23ab2e
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-07-27 21:33:05 +0200

    fix: updated styles, corrected styles

--- packages/frontend/app/templates/Support/components/Volunteering/Volunteering.jsx
+++ packages/frontend/app/templates/Support/components/Volunteering/Volunteering.jsx
@@ -1,11 +1,9 @@
 import Typography from "design-system/components/typography"
 import Feature from "design-system/patterns/feature"
 import Illustration from "design-system/components/illustration"
-import Title from "design-system/components/title"
-import Button from "design-system/components/button"
-import List from "design-system/components/list"
-import ListItem from "design-system/components/listItem"
+import TextBanner from "design-system/patterns/textBanner"
 import * as Styled from "./Volunteering.styled"
+import Button from "design-system/components/button"
 
 const features = [
   {
@@ -34,57 +32,60 @@ const features = [
   },
 ]
 
+const descriptions = [
+  {
+    description:
+      "Choć w prowadzenie Fundacji wkładamy całe serce, wciąż brakuje nam rąk do pracy. Poszukujemy ludzi o wielkim sercu, którzy chcą poświęcić swój czas i energię, aby poprawić życie bezdomnych zwierząt.",
+  },
+  {
+    description:
+      "Potrzebujemy wolontariuszy, którzy pomogą odzyskać naszym podopiecznym nadzieję na lepszy dom.",
+  },
+  {
+    description:
+      "Wolontariat w fundacji to nie tylko okazja do pomocy, ale także do zdobycia nowych doświadczeń i nawiązania pięknych relacji z bezdomnymi psami i kotami.",
+  },
+  {
+    description:
+      "Jeśli chcesz pomóc zwierzętom, ale nie wiesz jak to zrobić - dołącz do nas! Każde wsparcie jest dla nas cenne.",
+  },
+]
+
 const Volunteering = () => (
   <Styled.Wrapper>
     <Styled.ContainerSection as="section">
-      <Styled.Banner
-        heading="Zostań wolontariuszem"
-        subtitle="wolontariat"
-        tabletLayout="left"
-        mobileLayout="left"
-      />
-      <Styled.TextWrapper>
-        <Typography variant="bodyTitle">
-          Choć w prowadzenie Fundacji wkładamy całe serce, wciąż brakuje nam rąk
-          do pracy. Poszukujemy ludzi o wielkim sercu, którzy chcą poświęcić
-          swój czas i energię, aby poprawić życie bezdomnych zwierząt.
-        </Typography>
-        <Typography variant="bodyTitle">
-          Potrzebujemy wolontariuszy, którzy pomogą odzyskać naszym podopiecznym
-          nadzieję na lepszy dom.
-        </Typography>
-        <Typography variant="bodyTitle">
-          Wolontariat w fundacji to nie tylko okazja do pomocy, ale także do
-          zdobycia nowych doświadczeń i nawiązania pięknych relacji z bezdomnymi
-          psami i kotami.
-        </Typography>
-        <Typography variant="bodyTitle">
-          Jeśli chcesz pomóc zwierzętom, ale nie wiesz jak to zrobić - dołącz do
-          nas! Każde wsparcie jest dla nas cenne.
-        </Typography>
-      </Styled.TextWrapper>
-      <Styled.Img src="../support/volunteering-image.png" alt="" />
-      <Styled.FeatureWrapper>
-        <Styled.Title>
-          <Title text="Kogo szukamy?" variant="h3" />
-        </Styled.Title>
-        <List gap={20}>
-          {features.map(({ heading, description, illustration }) => (
-            <ListItem key={heading}>
-              <Feature heading={heading} illustration={illustration}>
+      <Styled.MainBanner>
+        <Styled.Img src="../support/volunteering-image.png" alt="" />
+        <Styled.ImageWrapper>
+          <Illustration name="heartLine" />
+        </Styled.ImageWrapper>
+          <Styled.TextWrapper>
+            <TextBanner
+            heading="Zostań wolontariuszem"
+            subtitle="Wolontariat"
+            tabletLayout="left"
+            mobileLayout="left"
+          />
+            {descriptions.map(({ description }) => (
+              <Typography variant="bodyTitle" key={description}>
                 {description}
-              </Feature>
-            </ListItem>
-          ))}
-        </List>
-      </Styled.FeatureWrapper>
-      <Styled.ImageWrapper>
-        <Illustration name="heartLine" />
-      </Styled.ImageWrapper>
+              </Typography>
+            ))}
+          </Styled.TextWrapper>
+        
+      </Styled.MainBanner>
+      <Styled.TitleText text="Kogo szukamy?" variant="h3" />
+      <Styled.Features>
+        {features.map(({ heading, description, illustration }) => (
+          <Feature heading={heading} illustration={illustration} key={heading}>
+            {description}
+          </Feature>
+        ))}
+      </Styled.Features>
+      <Styled.Button>
+        <Button text="Zgłoś się" size="medium" variant="fill" href="/kontakt" />
+      </Styled.Button>
     </Styled.ContainerSection>
-    <Styled.Button>
-      <Button text="Zgłoś się" size="medium" variant="fill" href="/kontakt" />
-    </Styled.Button>
   </Styled.Wrapper>
 )
 
--- packages/frontend/app/templates/Support/components/Volunteering/Volunteering.styled.js
+++ packages/frontend/app/templates/Support/components/Volunteering/Volunteering.styled.js
@@ -1,10 +1,9 @@
 import styled from "@emotion/styled"
 import Container from "design-system/components/container"
 import Image from "design-system/components/image"
-import TextBanner from "design-system/patterns/textBanner"
+import Title from "design-system/components/title"
 
 export const ContainerSection = styled(Container)`
-  display: grid;
   margin: 50px auto;
   position: relative;
 `
@@ -13,21 +12,14 @@ export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.neutrals[200]};
   padding-top: 10px;
   padding-bottom: 50px;
-  ${({ theme }) => theme.breakpoints.mobileLg} {
-    grid-template-columns: 1fr;
-    grid-template-rows: auto auto auto;
-    margin-bottom: 10px;
-  }
+
   ${({ theme }) => theme.breakpoints.tabletLg} {
-    margin: 0px auto;
     padding-top: 0px;
-    padding-bottom: 50px;
+    padding-bottom: 30px;
   }
 `
 
 export const Img = styled(Image)`
-  grid-column: 1;
-  grid-row: 1;
   width: 90%;
   height: 90%;
   ${({ theme }) => theme.breakpoints.tabletLg} {
@@ -39,86 +31,53 @@ export const Img = styled(Image)`
   }
   ${({ theme }) => theme.breakpoints.mobileLg} {
     margin-left: -25px;
+    height: 300px;
   }
 `
 export const TextWrapper = styled.div`
-  grid-column: 2;
-  grid-row: 1;
-  padding-top: 160px;
-  ${({ theme }) => theme.breakpoints.tabletLg} {
-    grid-column: 1;
-    grid-row: 3;
-    padding-left: 0px;
-    padding-top: 0px;
-    width: 80%;
-  }
+padding-top: 10px;
   ${({ theme }) => theme.breakpoints.mobileLg} {
-    grid-row: 4;
     width: 90%;
-    padding-top: 0px;
   }
 `
 
-export const Banner = styled(TextBanner)`
-  grid-column: 2;
-  grid-row: 1;
-  width: 100%;
+export const MainBanner = styled.div`
+  display: flex;
+  gap: 40px;
   ${({ theme }) => theme.breakpoints.tabletLg} {
-    grid-column: 1;
-    grid-row: 2;
-    padding-left: 0px;
-    padding-top: 270px;
-  }
-  ${({ theme }) => theme.breakpoints.mobileLg} {
-    padding-top: 140px;
-    grid-row: 3;
-  }
-`
-
-export const FeatureWrapper = styled.div`
-  grid-row: 3;
-  width: 115%;
-
-  ${({ theme }) => theme.breakpoints.tabletLg} {
-    grid-column: 1;
-    grid-row: 11;
-    width: 90%;
-  }
-  ${({ theme }) => theme.breakpoints.mobileLg} {
-    width: 90%;
+    flex-direction: column;
   }
 `
 
 export const ImageWrapper = styled.div`
   position: absolute;
   transform: rotate(25deg);
-  margin-top: 845px;
+  margin-top: 730px;
   right: 0px;
   width: 43%;
 
   ${({ theme }) => theme.breakpoints.tabletLg} {
+    position: relative;
+    margin-top: 0px;
     transform: rotate(0deg);
-    margin-left: 0px;
-    right: 250px;
-    margin-top: 650px;
+    right: 0px;
     width: 70%;
-    grid-column: 1;
-    grid-row: 1;
-  }
-  ${({ theme }) => theme.breakpoints.mobileLg} {
-    margin-top: 630px;
-    transform: rotate(0deg);
-    right: 100px;
   }
 `
 
-export const Title = styled.div`
+export const TitleText = styled(Title)`
   padding-top: 50px;
   padding-bottom: 30px;
 `
-
 export const Button = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 50px;
+  margin: 50px auto 0;
+`
+export const Features = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 700px;
+  margin-bottom: 60px;
 `
