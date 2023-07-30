6d4ae89b939a322919b3312365220678b6ecf418
amand <amanda.krempa1@gmail.com>
2023-07-30 15:58:59 +0200

    feat: financial statements

---
+++ packages/frontend/app/templates/About/components/FinancialStatements/FinancialStatements.jsx
@@ -0,0 +1,66 @@
+import { shape, arrayOf, string } from "prop-types"
+import React from "react"
+import TextBanner from "design-system/patterns/textBanner"
+import Typography from "design-system/components/typography"
+import Button from "design-system/components/button"
+import Icon from "design-system/components/icon"
+import Image from "design-system/components/image"
+
+import * as Styled from "./FinancialStatements.styled"
+
+const FinancialStatements = ({ data }) => (
+  <Styled.Container>
+    <Styled.FinancialDataWrapper>
+      <TextBanner
+        heading="Dane finansowe fundacji"
+        subtitle="Sprawdź"
+        mobileLayout="left"
+        tabletLayout="left"
+      >
+        <Styled.Description>
+          <Typography variant="bodyTitle">
+            Wierzymy, że zaufanie do fundacji jest podstawowym warunkiem
+            skutecznego pomagania. Przejrzystość naszej pracy i transparentność
+            wobec darczyńców cenimy sobie ponad wszystko.
+          </Typography>
+        </Styled.Description>
+      </TextBanner>
+      <Typography variant="h5">
+        Tutaj znajdziesz wszystkie sprawozdania z działalności fundacji:
+        <Styled.ButtonsContainer>
+          {data.map(({ name, url }) => (
+            <Button
+              variant="text"
+              text={name}
+              href={url}
+              key={name}
+              iconStart="download"
+            />
+          ))}
+        </Styled.ButtonsContainer>
+      </Typography>
+    </Styled.FinancialDataWrapper>
+    <Styled.TextWrapper>
+      <Typography variant="h4">
+        Dziękujemy za Wasze wsparcie <Icon name="heartFilled" size="large" />
+      </Typography>
+    </Styled.TextWrapper>
+    <Styled.ImageWrapper>
+      <Styled.ImageHeading variant="h6">
+        Nasze najważniejsze koszty:
+      </Styled.ImageHeading>
+      <Image src="/about/chart-desktop.png" alt="" />
+    </Styled.ImageWrapper>
+  </Styled.Container>
+)
+
+FinancialStatements.propTypes = {
+  data: arrayOf(
+    shape({
+      name: string,
+      url: string,
+    })
+  ).isRequired,
+}
+
+export default FinancialStatements
---
+++ packages/frontend/app/templates/About/components/FinancialStatements/FinancialStatements.styled.js
@@ -0,0 +1,86 @@
+import styled from "@emotion/styled"
+import Typography from "design-system/components/typography"
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-areas:
+    "financial image"
+    "text text";
+  grid-column-gap: 120px;
+  margin: 120px 0;
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "financial"
+      "image"
+      "text";
+    grid-gap: 0;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    max-width: 340px;
+  }
+`
+
+export const FinancialDataWrapper = styled.div`
+  max-width: 584px;
+  grid-area: financial;
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    max-width: 342px;
+  }
+`
+
+export const Description = styled.div`
+  margin-bottom: 60px;
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    margin-bottom: 40px;
+  }
+`
+
+export const ImageWrapper = styled.div`
+  grid-area: image;
+  align-self: flex-end;
+
+  img {
+    width: 539px;
+    height: 388px;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    img {
+      width: auto;
+      height: 292px;
+    }
+  }
+`
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 20px;
+  margin-bottom: 111px;
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    margin-bottom: 60px;
+  }
+`
+
+export const TextWrapper = styled.div`
+  grid-area: text;
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    margin-top: 60px;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    max-width: 211px;
+  }
+`
+
+export const ImageHeading = styled(Typography)`
+  margin-bottom: 10px;
+`
