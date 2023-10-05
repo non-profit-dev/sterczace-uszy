7669f81a66bed57804f5fc28605b0b907ac747f2
imklau <klaudiazawodnik@gmail.com>
2023-10-05 15:36:42 +0200

    chore(cypress): update viewport config

--- packages/frontend/e2e/cypress.config.js
+++ packages/frontend/e2e/cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     testIsolation: false,
-    viewportWidth: 1440,
-    viewportHeight: 920,
+    viewportWidth: 1600,
+    viewportHeight: 1200,
     scrollBehavior: false,
   },
 })
