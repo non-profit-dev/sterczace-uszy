855db89c08aef691a5835b9b957d7ca0aa6e2523
Amanda <97829893+AmandaKr@users.noreply.github.com>
2023-11-03 09:03:36 +0100

    feat: navigation with dropdowns (#207)
    Co-authored-by: imklau <klaudiazawodnik@gmail.com>

--- packages/design-system/blocks/navigation/Navigation.jsx
+++ packages/design-system/blocks/navigation/Navigation.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react"
+import { string, arrayOf, shape } from "prop-types"
 import Link from "next/link"
-
 import Button from "design-system/components/button"
 import Container from "design-system/components/container"
 import Icon from "design-system/components/icon"
 import List from "design-system/components/list"
 import ListItem from "design-system/components/listItem"
 import Logo from "design-system/components/logo"
-
+import { useState } from "react"
 import * as Styled from "./Navigation.styled"
 
 import data from "./data"
 
-const Navigation = () => {
+const Navigation = ({ navigationData }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [activeDropdown, setActiveDropdown] = useState(null)
+
+  const handleDropdownToggle = (index) => {
+    setActiveDropdown(activeDropdown === index ? null : index)
+  }
+
+  const handleHoverToggle = (index) => {
+    setActiveDropdown(index)
+  }
+  const handleMouseLeave = () => {
+    setIsOpen(false)
+    setActiveDropdown(null)
+  }
 
   return (
     <>
@@ -25,19 +37,69 @@ const Navigation = () => {
             </Link>
 
             <Styled.Menu>
-              {data.map((item) => (
-                <li key={item.title}>
-                  <Button
-                    as="a"
-                    href={item.href}
-                    text={item.title}
-                    variant="text"
-                    size="xsmall"
-                    color={
-                      item.href === "/pliki-do-pobrania" ? "primary" : "black"
-                    }
-                  />
-                </li>
+              {navigationData.map((item, index) => (
+                <Styled.MenuButton
+                  key={item.title}
+                  onMouseEnter={() => handleHoverToggle(index)}
+                  onMouseLeave={() => handleMouseLeave()}
+                >
+                  {item.links ? (
+                    <>
+                      <Styled.DropdownButton>
+                        <Button
+                          onClick={() => handleDropdownToggle(index)}
+                          text={item.title}
+                          href={item.links.href}
+                          variant="text"
+                          size="xsmall"
+                          color={
+                            item.href === "/pliki-do-pobrania"
+                              ? "primary"
+                              : "black"
+                          }
+                        />
+                        <Styled.ChevronIcon
+                          name="chevronDown"
+                          size="small"
+                          isActive={activeDropdown === index}
+                        />
+                      </Styled.DropdownButton>
+                      {activeDropdown === index && (
+                        <Styled.DropdownWrapper>
+                          <Styled.Dropdown>
+                            <List gap={16}>
+                              {item.links.map((link) => (
+                                <ListItem key={link.title}>
+                                  <Button
+                                    href={link.href}
+                                    text={link.title}
+                                    variant="text"
+                                    size="xsmall"
+                                    color={
+                                      link.href === "/pliki-do-pobrania"
+                                        ? "primary"
+                                        : "black"
+                                    }
+                                  />
+                                </ListItem>
+                              ))}
+                            </List>
+                          </Styled.Dropdown>
+                        </Styled.DropdownWrapper>
+                      )}
+                    </>
+                  ) : (
+                    <Button
+                      text={item.title}
+                      variant="text"
+                      href={item.href}
+                      size="xsmall"
+                      color={
+                        item.href === "/pliki-do-pobrania" ? "primary" : "black"
+                      }
+                    />
+                  )}
+                </Styled.MenuButton>
               ))}
             </Styled.Menu>
 
@@ -52,28 +114,71 @@ const Navigation = () => {
 
         {isOpen ? (
           <Styled.MobileNavigation>
-            <List gap={24}>
-              {data.map((item) => (
-                <ListItem key={item.title}>
-                  <Button
+            {navigationData.map((item) => (
+              <div key={item.title}>
+                {item.links ? (
+                  <Styled.NavigationAccordion
+                    heading={item.title}
+                    activeColor="primary"
+                    isNavigationAccordion
+                  >
+                    <Styled.AccordionLinks>
+                      {item.links.map((link) => (
+                        <Button
+                          key={link.title}
+                          as="a"
+                          text={link.title}
+                          href={link.href}
+                          variant="text"
+                          size="medium"
+                          color={
+                            link.href === "/pliki-do-pobrania"
+                              ? "primary"
+                              : "black"
+                          }
+                        />
+                      ))}
+                    </Styled.AccordionLinks>
+                  </Styled.NavigationAccordion>
+                ) : (
+                  <Styled.AccordionButton
                     as="a"
-                    href={item.href}
                     text={item.title}
+                    href={item.href}
                     variant="text"
                     size="medium"
                     color={
                       item.href === "/pliki-do-pobrania" ? "primary" : "black"
                     }
                   />
-                </ListItem>
-              ))}
-            </List>
+                )}
+              </div>
+            ))}
           </Styled.MobileNavigation>
         ) : null}
       </Styled.Navigation>
       {isOpen ? <Styled.Overlay onClick={() => setIsOpen(!isOpen)} /> : null}
     </>
   )
+}
+
+Navigation.propTypes = {
+  navigationData: arrayOf(
+    shape({
+      title: string,
+      href: string,
+      links: arrayOf(
+        shape({
+          title: string,
+          href: string,
+        })
+      ),
+    })
+  ),
+}
+
+Navigation.defaultProps = {
+  navigationData: data,
 }
 
 export default Navigation
--- packages/design-system/blocks/navigation/Navigation.styled.js
+++ packages/design-system/blocks/navigation/Navigation.styled.js
@@ -1,3 +1,6 @@
+import Icon from "design-system/components/icon"
+import Accordion from "design-system/components/accordion"
+import Button from "design-system/components/button"
 import styled from "@emotion/styled"
 
 export const Navigation = styled.nav`
@@ -6,7 +9,7 @@ export const Navigation = styled.nav`
   z-index: 2;
   width: 100%;
   background: ${({ theme }) => theme.colors.neutrals[100]};
-  padding: 28px 0;
+  padding: 22px 0;
 
   ${({ theme }) => theme.breakpoints.tabletLg} {
     background: ${({ theme }) => theme.colors.neutrals[200]};
@@ -14,6 +17,41 @@ export const Navigation = styled.nav`
 
   ${({ theme }) => theme.breakpoints.tablet} {
     padding: 14px 0;
+  }
+`
+
+export const Dropdown = styled.div`
+  background: ${({ theme }) => theme.colors.neutrals[100]};
+  min-width: 205px;
+  padding: 16px;
+  display: block;
+  box-shadow: -8px 3px 14px 0px rgba(91, 91, 91, 0.2);
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    display: none;
+  }
+`
+
+export const DropdownWrapper = styled.div`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  z-index: 1;
+  padding-top: 14px;
+`
+
+export const ChevronIcon = styled(Icon)`
+  transform: ${(props) => (props.isActive ? "rotate(180deg)" : "")};
+  transition: 400ms ease;
+`
+
+export const DropdownButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    display: none;
   }
 `
 
@@ -26,6 +64,7 @@ export const NavigationRow = styled.div`
 export const Menu = styled.ul`
   display: flex;
   gap: 48px;
+  position: relative;
 
   ${({ theme }) => theme.breakpoints.tabletLg} {
     display: none;
@@ -44,7 +83,6 @@ export const MenuIcon = styled.button`
     display: inline-flex;
   }
 `
-
 export const MobileNavigation = styled.div`
   display: none;
   position: absolute;
@@ -52,12 +90,13 @@ export const MobileNavigation = styled.div`
   right: 0;
   width: 100%;
   max-width: 480px;
-  padding: 40px 60px;
+  padding: 20px;
   background: ${({ theme }) => theme.colors.neutrals[200]};
 
   ${({ theme }) => theme.breakpoints.tabletLg} {
     display: block;
     max-width: unset;
+    padding: 24px 60px;
   }
 
   ${({ theme }) => theme.breakpoints.mobile} {
@@ -78,5 +117,24 @@ export const Overlay = styled.div`
 
   ${({ theme }) => theme.breakpoints.tabletLg} {
     display: block;
+  }
+`
+export const NavigationAccordion = styled(Accordion)`
+  background: ${({ theme }) => theme.colors.neutrals[200]};
+`
+
+export const AccordionButton = styled(Button)`
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    padding: 20px 0;
+  }
+`
+
+export const AccordionLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  a span {
+    font-weight: 400;
   }
 `
