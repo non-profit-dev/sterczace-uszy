6247eab322bbeb5f172567241aef0cc950e294e3
imklau <klaudiazawodnik@gmail.com>
2023-11-10 10:59:51 +0100

    feat: replace hero image

--- packages/frontend/app/templates/InternationalAdoption/components/Hero/Hero.styled.js
+++ packages/frontend/app/templates/InternationalAdoption/components/Hero/Hero.styled.js
@@ -2,14 +2,9 @@ import styled from "@emotion/styled"
 
 export const Header = styled.header`
   margin: 0 auto;
-  padding-top: 380px;
-  padding-bottom: 200px;
-  background: linear-gradient(
-      52deg,
-      rgba(0, 0, 0, 0.51) 34.13%,
-      rgba(0, 0, 0, 0) 50.9%
-    ),
-    url("/adoption/international-adoption/hero-background.png");
+  padding-top: 280px;
+  padding-bottom: 280px;
+  background: url("/adoption/international-adoption/hero-background.png");
   background-repeat: no-repeat;
   background-position: top;
   background-size: cover;
@@ -34,13 +29,15 @@ export const HeaderWrapper = styled.div`
 export const HeroMobileBackground = styled.div`
   display: none;
   height: 560px;
-  background: url("/adoption/international-adoption/hero-background-tablet.png");
+  background: url("/adoption/international-adoption/hero-background.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+
   ${({ theme }) => theme.breakpoints.tablet} {
     display: block;
   }
+
   ${({ theme }) => theme.breakpoints.mobileLg} {
     background-image: url("/adoption/international-adoption/hero-background-mobile.png");
     height: 380px;
