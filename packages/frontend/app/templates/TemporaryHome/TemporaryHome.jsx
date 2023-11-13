7305ed860d6de296ba4d04fa58acf23256a52eda
amand <amanda.krempa1@gmail.com>
2023-11-14 00:51:28 +0100

    feat: temporary home page

---
+++ packages/frontend/app/templates/TemporaryHome/TemporaryHome.jsx
@@ -0,0 +1,24 @@
+import Navigation from "design-system/blocks/navigation"
+import Footer from "design-system/blocks/footer"
+import CtaBanner from "design-system/patterns/ctaBanner"
+import TemporaryHomeSection from "./components/temporary-home/TemporaryHome"
+import Page from "../Page"
+import Hero from "./components/hero/Hero"
+
+const TemporaryHome = () => {
+  return (
+    <Page title="Pomagaj za darmo">
+      <Navigation />
+      <Hero />
+      <TemporaryHomeSection />
+      <CtaBanner
+        heading="Masz pytania lub wątpliwości?"
+        buttonText="Skontaktuj się z nami"
+        buttonHref="/kontakt"
+        mobileLayout="left"
+      />
+      <Footer />
+    </Page>
+  )
+}
+export default TemporaryHome
