0f83bfab3142b55c2683203d9ef72717ce7732b2
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2024-02-12 23:09:24 +0100

    feat: terms of payment page

---
+++ packages/frontend/app/templates/TermsOfPayment/components/Content/Content.styled.js
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled"
+
+export const Content = styled.div`
+  padding: 120px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 1062px;
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    padding: 50px 0;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    padding: 30px 0;
+  }
+`
+
+export const Image = styled.img`
+  width: 100%;
+  box-shadow: 2px 3px 9px 0px rgba(0, 0, 0, 0.1);
+  margin-top: 36px;
+  margin-bottom: 28px;
+
+  ${({ theme }) => theme.breakpoints.tablet} {
+    height: 90px;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    height: 42px;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobile} {
+    height: 42px;
+  }
+`
---
+++ packages/frontend/app/templates/TermsOfPayment/components/Content/index.js
@@ -0,0 +1,3 @@
+import TemporaryHome from "./Content"
+
+export default TemporaryHome
