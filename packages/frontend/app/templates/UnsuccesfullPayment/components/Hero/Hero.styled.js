89a0a4f8a99107eb9a71c4c3f9a933f0c5eebe6a
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2024-03-09 22:45:55 +0100

    feat: unsuccessful payment page

---
+++ packages/frontend/app/templates/UnsuccesfullPayment/components/Hero/Hero.styled.js
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled"
+
+export const Header = styled.header`
+  padding: 120px 0;
+  background: linear-gradient(180deg, #faf4f4 0%, #faf4f4 86.11%, #fefcfc 100%);
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    padding: 100px 0;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    padding: 60px 0;
+  }
+`
+
+export const HeaderWrapper = styled.div`
+  max-width: 762px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  text-align: center;
+  margin-inline: auto;
+
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    max-width: 432px;
+    gap: 30px;
+  }
+
+  ${({ theme }) => theme.breakpoints.mobileLg} {
+    gap: 10px;
+  }
+`
+
+export const ParagraphWrapper = styled.div`
+  max-width: 740px;
+  margin-top: 20px;
+  text-align: center;
+  margin-inline: auto;
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    max-width: 470px;
+  }
+`
+export const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-inline: auto;
+  width: 100%;
+  margin-top: 80px;
+  ${({ theme }) => theme.breakpoints.tabletLg} {
+    margin-top: 40px;
+  }
+`
+
+export const Image = styled.img`
+  width: 100%;
+  max-height: 480px;
+  max-width: 721px;
+`
