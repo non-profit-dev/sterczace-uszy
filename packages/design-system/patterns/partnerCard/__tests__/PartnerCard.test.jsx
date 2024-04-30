fbfe1336fe6331119cfdde2189a841932a136d42
imklau <klaudiazawodnik@gmail.com>
2024-04-30 14:28:34 +0200

    test: update

--- packages/design-system/patterns/partnerCard/__tests__/PartnerCard.test.jsx
+++ packages/design-system/patterns/partnerCard/__tests__/PartnerCard.test.jsx
@@ -2,9 +2,20 @@ import { render, screen } from "design-system/test-utils"
 
 import PartnerCard from "design-system/patterns/partnerCard"
 
+const props = {
+  title: "Partner Card Title",
+  discount: "-20%",
+  promoCode: "123",
+}
+
 describe(`PartnerCard`, () => {
   it(`renders with default properties`, () => {
-    render(<PartnerCard>test</PartnerCard>)
-    expect(screen.getByText(test)).toBeInTheDocument()
+    render(<PartnerCard title={props.title} />)
+    expect(screen.getByText(props.title)).toBeInTheDocument()
+  })
+
+  it(`renders the button for promo code if it's provided`, () => {
+    render(<PartnerCard {...props} />)
+    expect(screen.getByText(`Kopiuj kod ${props.discount}`)).toBeInTheDocument()
   })
 })
